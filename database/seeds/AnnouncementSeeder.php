584c39ed0b23427a077cf3079219e4fb48f239ff
Ian Cowan <ian@cowanemail.com>
2018-11-21 16:54:41 -0500

    Added announcement seeder

---
+++ database/seeds/AnnouncementSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AnnouncementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $time = Carbon::now();
+        DB::table('announcement')->insert([
+             'body' => null
+         ]);
+    }
+}
