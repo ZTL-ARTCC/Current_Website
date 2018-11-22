bd85ea20116b1822f4091f4e6364e00359f41f33
Ian Cowan <ian@cowanemail.com>
2018-11-22 11:29:52 -0500

    Fixed login on production

--- app/Http/Controllers/RosterController.php
+++ app/Http/Controllers/RosterController.php
@@ -28,11 +28,18 @@ class RosterController extends Controller
         if(Auth::check()) {
             return redirect('/')->with('error', 'You are already logged in.');
         }
-
-        if(!Auth::check() && !isset($_GET['token'])) {
-            $_SESSION['redirect'] = "http://144.202.31.238";
-            header("Location: https://login.vatusa.net/uls/v2/login?fac=ZTL&dev=1");
-            exit;
+        if(Config::get('app.debug') == false) {
+            if(!Auth::check() && !isset($_GET['token'])) {
+                $_SESSION['redirect'] = Config::get('app.url');
+                header("Location: https://login.vatusa.net/uls/v2/login?fac=".Config::get('vatusa.facility'));
+                exit;
+            }
+        } else {
+            if(!Auth::check() && !isset($_GET['token'])) {
+                $_SESSION['redirect'] = Config::get('app.url');
+                header("Location: https://login.vatusa.net/uls/v2/login?fac=".Config::get('vatusa.facility')."&dev=1");
+                exit;
+            }
         }
 
         $token = $_GET['token'];
