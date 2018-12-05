b5777e813827c8ccb7664fc7b33770505c454565
Ian Cowan <ian@cowanemail.com>
2018-12-05 18:09:03 -0500

    Merry Christmas, 2018

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,9 @@
         <link rel="stylesheet" href="/css/Footer-white.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
+        {{-- Merry Christmas --}}
+        <script src="/js/snowstorm.js"></script>
+
         {{-- Bootstrap --}}
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -23,7 +26,7 @@
 
         {{-- Google reCAPTCHA v2 --}}
         <script src='https://www.google.com/recaptcha/api.js'></script>
-        
+
         {{-- Title --}}
         <title>
             @yield('title') | ZTL ARTCC
