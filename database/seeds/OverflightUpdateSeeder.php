21e3ad668e7746f748ffb0a038e2b776c82cd927
Ian Cowan <ian@cowanemail.com>
2019-01-09 15:57:43 -0500

    Added seeder for overflights

---
+++ database/seeds/OverflightUpdateSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class OverflightSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('flights_within_artcc_updates')->insert([
+             'created_at' => null,
+             'updated_at' => null
+         ]);
+    }
+}
