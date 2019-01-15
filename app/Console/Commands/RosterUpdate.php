5b34dcbfb82873ec46391f9014c07ff30599c70e
Ian Cowan <ian@cowanemail.com>
2019-01-14 21:03:42 -0500

    Initially assign moodle roles

--- app/Console/Commands/RosterUpdate.php
+++ app/Console/Commands/RosterUpdate.php
@@ -52,6 +52,7 @@ class RosterUpdate extends Command
         foreach($roster as $r) {
             if(User::find($r->cid) !== null) {
                 $user = User::find($r->cid);
+                $rating_old = $user->rating_id;
                 $user->fname = $r->fname;
                 $user->lname = $r->lname;
                 $user->email = $r->email;
@@ -59,6 +60,36 @@ class RosterUpdate extends Command
                 $user->visitor = '0';
                 $user->added_to_facility = substr($r->facility_join, 0, 10).' '.substr($r->facility_join, 11, 8);
                 $user->save();
+
+                //Assigns role in moodle database
+                //if($rating_old != $r->rating) {
+                    $old_role = DB::table('mdl_role_assignments')->where('userid', $user->id)->where('roleid', '!=', 14)->where('roleid', '!=', 15)->where('roleid', '!=', 16)->where('roleid', '!=', 17)->first();
+                    $old_role->delete();
+
+                    if($user->rating_id == 1) {
+                        $mdl_rating = 18;
+                    } elseif($user->rating_id == 2) {
+                        $mdl_rating = 9;
+                    } elseif($user->rating_id == 3) {
+                        $mdl_rating = 10;
+                    } elseif($user->rating_id == 4) {
+                        $mdl_rating = 11;
+                    } elseif($user->rating_id == 5) {
+                        $mdl_rating = 12;
+                    } elseif($user->rating_id == 7 || $user->rating_id == 11 || $user->rating_id == 12) {
+                        $mdl_rating = 13;
+                    } elseif($user->rating_id == 8 || $user->rating_id == 10) {
+                        $mdl_rating = 14;
+                    } else {
+                        $mdl_rating = 0;
+                    }
+
+                    DB::table('mdl_role_assignments')->insert([
+                        'roleid' => $mdl_rating,
+                        'contextid' => 26,
+                        'userid' => $user->id
+                    ]);
+                //}
             } else {
                 $user = new User;
                 $user->id = $r->cid;
@@ -83,6 +114,31 @@ class RosterUpdate extends Command
                 $user->status = '1';
                 $user->added_to_facility = substr($r->facility_join, 0, 10).' '.substr($r->facility_join, 11, 8);
                 $user->save();
+
+                //Assigns role in moodle database
+                if($user->rating_id == 1) {
+                    $mdl_rating = 18;
+                } elseif($user->rating_id == 2) {
+                    $mdl_rating = 9;
+                } elseif($user->rating_id == 3) {
+                    $mdl_rating = 10;
+                } elseif($user->rating_id == 4) {
+                    $mdl_rating = 11;
+                } elseif($user->rating_id == 5) {
+                    $mdl_rating = 12;
+                } elseif($user->rating_id == 7 || $user->rating_id == 11 || $user->rating_id == 12) {
+                    $mdl_rating = 13;
+                } elseif($user->rating_id == 8 || $user->rating_id == 10) {
+                    $mdl_rating = 14;
+                } else {
+                    $mdl_rating = 0;
+                }
+
+                DB::table('mdl_role_assignments')->insert([
+                    'roleid' => $mdl_rating,
+                    'contextid' => 26,
+                    'userid' => $user->id
+                ]);
 
                 //Assigns controller initials
                 $user = User::find($r->cid);
