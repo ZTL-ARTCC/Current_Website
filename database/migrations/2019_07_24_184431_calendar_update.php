61aa757b961ae6500169ab6dd0542601941a5fe2
Jordan <jordiedevs@gmail.com>
2019-07-25 09:16:22 -0400

    Fixed migration typo

--- database/migrations/2019_07_24_184431_calendar_update.php
+++ database/migrations/2019_07_24_184431_calendar_update.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CalenderUpdate extends Migration
+class CalendarUpdate extends Migration
 {
     /**
      * Run the migrations.
