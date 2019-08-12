bd4076946bc36e3bea580024ec9690818a25cda4
Ian Cowan <ian@cowanemail.com>
2019-08-12 20:12:17 +0100

    Fix solo certs

--- app/Console/Commands/UpdateSoloCerts.php
+++ app/Console/Commands/UpdateSoloCerts.php
@@ -47,36 +47,38 @@ class UpdateSoloCerts extends Command
         $solo_certs = json_decode($res->getBody());
 
         foreach($solo_certs as $s) {
-            if($s->position == 'ATL_CTR') {
-                $current_cert = SoloCert::where('cid', $s->cid)->where('status', 0)->first();
-                if(!$current_cert) {
-                    $cert = new SoloCert;
-                    $cert->cid = $s->cid;
-                    $cert->pos = 2;
-                    $cert->expiration = $s->expires;
-                    $cert->status = 0;
-                    $cert->save();
+            if(! ($s === true || $s === false)) {
+                if ($s->position == 'ATL_CTR') {
+                    $current_cert = SoloCert::where('cid', $s->cid)->where('status', 0)->first();
+                    if (!$current_cert) {
+                        $cert = new SoloCert;
+                        $cert->cid = $s->cid;
+                        $cert->pos = 2;
+                        $cert->expiration = $s->expires;
+                        $cert->status = 0;
+                        $cert->save();
 
-                    $user = User::find($s->cid);
-                    $user->ctr = 99;
-                    $user->save();
-                }
-            } elseif(substr($s->position, -3) == 'APP') {
-                $hcontrol = User::where('visitor', 0)->get();
-                foreach($hcontrol as $h) {
-                    if($s->cid == $h->id) {
-                        $current_cert = SoloCert::where('cid', $s->cid)->where('status', 0)->first();
-                        if(!$current_cert) {
-                            $cert = new SoloCert;
-                            $cert->cid = $s->cid;
-                            $cert->pos = 2;
-                            $cert->expiration = $s->expires;
-                            $cert->status = 0;
-                            $cert->save();
+                        $user = User::find($s->cid);
+                        $user->ctr = 99;
+                        $user->save();
+                    }
+                } elseif (substr($s->position, -3) == 'APP') {
+                    $hcontrol = User::where('visitor', 0)->get();
+                    foreach ($hcontrol as $h) {
+                        if ($s->cid == $h->id) {
+                            $current_cert = SoloCert::where('cid', $s->cid)->where('status', 0)->first();
+                            if (!$current_cert) {
+                                $cert = new SoloCert;
+                                $cert->cid = $s->cid;
+                                $cert->pos = 2;
+                                $cert->expiration = $s->expires;
+                                $cert->status = 0;
+                                $cert->save();
 
-                            $user = $h;
-                            $user->app = 99;
-                            $user->save();
+                                $user = $h;
+                                $user->app = 99;
+                                $user->save();
+                            }
                         }
                     }
                 }
@@ -88,7 +90,7 @@ class UpdateSoloCerts extends Command
         $certs = SoloCert::get();
 
         foreach($certs as $c) {
-            if($c->expiration == $today && $c->status == 0) {
+            if($c->expiration <= $today && $c->status == 0) {
                 Mail::send('emails.solo_expire', ['c' => $c], function($message){
                     $message->from('solocerts@notams.ztlartcc.org', 'ZTL Solo Certifications')->subject('Solo Certification Expired');
                     $message->to('ta@ztlartcc.org');
