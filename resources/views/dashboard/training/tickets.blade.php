0941aa4cdb3b52a13f3214086495673bb12e5316
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2019-09-02 15:12:54 +0100

    Update tickets.blade.php

--- resources/views/dashboard/training/tickets.blade.php
+++ resources/views/dashboard/training/tickets.blade.php
@@ -39,19 +39,9 @@ Training Tickets
             <div class="col-sm-1">
                 <button class="btn btn-primary" action="submit">Search</button>
             </div>
-            <div class="col-sm-1">
+        </div>
     {!! Form::close() !!}
-    <center>OR</center>
-    {!! Form::open(['url' => '/dashboard/training/tickets/search']) !!}
-            </div>
-            <div class="col-sm-3">
-                {!! Form::select('cid', $controllers, null, ['placeholder' => 'Select Mentor', 'class' => 'form-control']) !!}
-            </div>
-            <div class="col-sm-1">
-                <button class="btn btn-primary" action="submit">Search</button>
-            </div>
-            </div>
-    {!! Form::close() !!}
+
 
     @if($search_result != null)
         <hr>
