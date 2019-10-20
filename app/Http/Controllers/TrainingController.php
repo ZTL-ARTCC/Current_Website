14fcca2e29f0019c540b4fcbc07df6440bf780f0
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2019-10-20 14:17:38 +0100

    Update TrainingController.php

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -4,6 +4,6 @@ namespace App\Http\Controllers;
 class TrainingController extends Controller {
     public function showMentAvail()
 	{
-		return View('dashboard.training.sch.mentoravail')->with('availability', $availability);
+		return View('dashboard.training.sch.mtr_avail')->with('availability', $availability);
     }
 }
