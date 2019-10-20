0a913ba41237c10ec574d8def285773634ba7f69
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2019-10-20 17:10:28 +0100

    Create MentorController.php

---
+++ app/Http/Controllers/MentorController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\MentorAvail;
+use App\User;
+use Auth;
+use Carbon\Carbon;
+
+class TrainingController extends Controller {
+    public function postAvail()
+	{
+		$mentor_id = Auth::id();
+		$slots = Input::get('slots');
+		$today = new Carbon("midnight today", 'America/New_York');
+
+		$availability = MentorAvail::where('mentor_id', '=', $mentor_id)->where('slot', '>=', $today)->get();
+
+		if (!$slots) $slots = [];
+
+		// Slots to be added
+
+		$new_slots = array_diff($slots, $availability->Pluck('slot')->toArray());
+		// Slots to be deleted
+		$old_slots = array_diff($availability->Pluck('slot')->toArray(), $slots);
+
+		foreach ($new_slots as $slot) {
+			MentorAvail::create([
+				'mentor_id' => $mentor_id,
+				'slot' => $slot,
+			]);
+		}
+
+		MentorAvail::where('mentor_id', '=', $mentor_id)->whereIn('slot', $old_slots)->delete();
+		
+
+		
+    }
+}
