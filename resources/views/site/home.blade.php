b8a29e5aecf1286ae369417f9638022c0d620a3f
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2019-11-06 17:31:50 +0000

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,6 +4,7 @@
 Home
 @endsection
 
+
 @section('content')
 <div class="jumbotron" style="background-image:url(/photos/ZTL_Banner.jpg); background-size:cover; background-repeat:no-repeat;">
     <div class="container">
