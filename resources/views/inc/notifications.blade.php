caff99a7e01c01b2df4d4b7f10af109cd6bff1b5
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2019-11-09 19:37:38 +0000

    Update notifications.blade.php

--- resources/views/inc/notifications.blade.php
+++ resources/views/inc/notifications.blade.php
@@ -1,14 +1,14 @@
 @if(
-    Auth::user()->hasRole('ins') && App\Training\Ots::where('status', 0)->get()->count() > 0 || Auth::user()->hasRole('atm') && App\Mship\Ots::where('status', 0)->get()->count() > 0 ||
-    App\Mship\Ots::where('status', 1)->where('ins_id', Auth::id())->get()->count() > 0 ||
-    App\Mship\TrainingTicket::where('created_at', '>=', Carbon\Carbon::now()->subHours(24))->where('controller_id', Auth::id())->first() != null ||
+    Auth::user()->hasRole('ins') && App\Training\Ots::where('status', 0)->get()->count() > 0 || Auth::user()->hasRole('atm') && App\Training\Ots::where('status', 0)->get()->count() > 0 ||
+    App\Training\Ots::where('status', 1)->where('ins_id', Auth::id())->get()->count() > 0 ||
+    App\Training\TrainingTicket::where('created_at', '>=', Carbon\Carbon::now()->subHours(24))->where('controller_id', Auth::id())->first() != null ||
     count(App\Mship\Incident::where('status', 0)->get()) > 0 && Auth::user()->can('snrStaff') ||
     count(App\Mship\Feedback::where('status', 0)->get()) > 0 && Auth::user()->can('snrStaff') ||
     count(App\Mship\Visitor::where('status', 0)->get()) > 0 && Auth::user()->can('snrStaff')
     )
     <hr>
     <center><h4><i>Notifications</i></h4></center>
-    @if(Auth::user()->hasRole('ins') && App\Training\Ots::where('status', 0)->get()->count() > 0 || Auth::user()->hasRole('atm') && App\Ots::where('status', 0)->get()->count() > 0)
+    @if(Auth::user()->hasRole('ins') && App\Training\Ots::where('status', 0)->get()->count() > 0 || Auth::user()->hasRole('atm') && App\Training\Ots::where('status', 0)->get()->count() > 0)
         <br>
         <div class="alert alert-success">
             There is a <b>new OTS recommendation</b> that is waiting to be accepted. View the <a href="/dashboard/training/ots-center">OTS Center</a> to view more information.
