20644f538b03366ecbb80b5be29c276ce78f1280
TheRealJBomb <jshothd@gmail.com>
2020-09-14 21:03:06 -0700

    attempted to create function

--- resources/views/dashboard/training/tickets.blade.php
+++ resources/views/dashboard/training/tickets.blade.php
@@ -4,6 +4,19 @@
 Training Tickets
 @endsection
 
+<<?php  function ShowTicketInList() { ?>
+  <tr>
+      <td><a href="/dashboard/training/tickets/view/{{ $t->id }}">{{ $t->date }}</a></td>
+      <td>{{ $t->trainer_name }}</td>
+      <td>{{ $t->position_name }}</td>
+      <td>{{ $t->type_name }}</td>
+      <td>{{ $t->start_time }}z</td>
+      <td>{{ $t->end_time }}z</td>
+      <td data-toggle="tooltip" title="{{ $t->ins_comments }}">{{ str_limit($t->ins_comments, 40, '...') }}</td>
+
+  </tr>
+<?php } ?>
+
 @section('content')
 <div class="container-fluid" style="background-color:#F0F0F0;">
     &nbsp;
@@ -97,7 +110,7 @@ Training Tickets
                             <td data-toggle="tooltip" title="{{ $t->ins_comments }}">{{ str_limit($t->ins_comments, 40, '...') }}</td>
 
                         </tr>
-
+                    
                     @elseif($t->position > 99 && $t->position < 109)
                     <tr>
                         <td><a href="/dashboard/training/tickets/view/{{ $t->id }}">{{ $t->date }}</a></td>
