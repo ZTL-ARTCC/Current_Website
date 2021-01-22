e0a897a6c3a6f35ee0e2cd2d8fc63f6e885ba579
TheRealJBomb <43980009+TheRealJBomb@users.noreply.github.com>
2021-01-21 23:31:28 -0800

    Update AdminDash.php
    now copies the `remove_visitor` email to the DATM when visitor is removed from the roster.

--- app/Http/Controllers/AdminDash.php
+++ app/Http/Controllers/AdminDash.php
@@ -833,7 +833,7 @@ class AdminDash extends Controller
             
              Mail::send('emails.remove_visitor', ['user' => $user], function($message) use ($user){
                         $message->from('datm@ztlartcc.org', 'vZTL ARTCC Staff')->subject('Notification of ZTL Roster Removal');
-                        $message->to($user->email);
+                        $message->to($user->email)->cc('datm@ztlartcc.org');
              });
 
             return redirect('/dashboard/controllers/roster')->with('success', 'The visitor has been removed successfully.');
