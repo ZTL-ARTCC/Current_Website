5c26004b9425875a4f8432219c0afd6bd12bfd10
TheRealJBomb <43980009+TheRealJBomb@users.noreply.github.com>
2021-03-10 12:21:43 -0800

    Update RosterUpdate.php

--- app/Console/Commands/RosterUpdate.php
+++ app/Console/Commands/RosterUpdate.php
@@ -179,8 +179,7 @@ class RosterUpdate extends Command
         $res = $client->get('https://api.vatusa.net/v2/facility/'.Config::get('vatusa.facility').'/roster?apikey='.Config::get('vatusa.api_key'));
         $users = User::where('visitor', '0')->where('status', '1')->get()->pluck('id');
         if ($res->getStatusCode() == "200") {
-            $roster = json_decode($res->getBody());
-            $roster->data;
+            $roster = json_decode($res->data);
         } else {
 
             exit(1);
