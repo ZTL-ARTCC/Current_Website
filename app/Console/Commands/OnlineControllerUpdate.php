b4c4cf185e6c4a344c2ce8824e0a6a78ec4577dd
Jeffrey Sydenham <jsydenham@ntlworld.com>
2021-04-08 03:31:06 +0100

    Update to use Json
    trying to update old code to work with new system

--- app/Console/Commands/OnlineControllerUpdate.php
+++ app/Console/Commands/OnlineControllerUpdate.php
@@ -6,6 +6,7 @@ use App\ATC;
 use App\ControllerLog;
 use App\ControllerLogUpdate;
 use Carbon\Carbon;
+use GuzzleHttp\Client;
 use DB;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -27,7 +28,7 @@ class OnlineControllerUpdate extends Command
      */
     protected $description = 'Retrieves all the online controllers and records them in the database.';
 
-    protected $statusUrl = "http://status.vatsim.net/status.txt";
+    protected $statusUrl = "https://data.vatsim.net/v3/vatsim-data.json";
 
     protected $facilities = [
 		/* BRAVO */
@@ -66,32 +67,15 @@ class OnlineControllerUpdate extends Command
 
 		DB::table('online_atc')->truncate();
 
-		$client_section = false;
-		foreach ($statsData as $line)
+		
+		foreach ($statsData->controllers as $line)
 		{
-			$line = trim($line);
-
-			if (strpos($line, ";") === 0)
-				continue;
-
-			if (!$client_section && strpos($line, "UPDATE") === 0)
-			{
-				list($command, $time) = explode("=", $line);
-				$time = trim($time);
-				$time = preg_replace("/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})$/", "$1-$2-$3 $4:$5:$6", $time);
-				$lastUpdated = strtotime($time);
-
-			}
-
-			if ($client_section && $line == "!SERVERS:")
-				$client_section = false;
-
-			if ($client_section)
-			{
-				list($position, $cid, $name, $clienttype, $frequency, $latitude, $longitude, $altitude, $groundspeed, $planned_aircraft, $planned_tascruise, $planned_depairport, $planned_altitude, $planned_destairport, $server, $protrevision, $rating, $transponder, $facilitytype, $visualrange, $planned_revision, $planned_flighttype, $planned_deptime, $planned_actdeptime, $planned_hrsenroute, $planned_minenroute, $planned_hrsfuel, $planned_minfuel, $planned_altairport, $planned_remarks, $planned_route, $planned_depairport_lat, $planned_depairport_lon, $planned_destairport_lat, $planned_destairport_lon, $atis_message, $time_last_atis_received, $time_logon, $heading) = explode(":", $line);
-
-				$is_controller = $clienttype == "ATC" && strpos($position, "OBS") === false && $rating != '1' && $facilitytype != '0' && strpos($position, "SAVF") === false && strpos($position, "SAVC") === false;
-				if (!$is_controller) continue;
+		
+				$position = $line->callsign;
+                $cid = $line->cid;
+                $name = $line->name;
+                $frequency = $line->frequency;
+                $time_logon = $line->logon_time;
 
 				foreach ($this->facilities as $facility)
 				{
@@ -102,7 +86,11 @@ class OnlineControllerUpdate extends Command
 
 				if ($is_controller) {
 					// Found an ATC user
-					$time_logon = strtotime(preg_replace("/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})$/", "$1-$2-$3 $4:$5:$6", $time_logon));
+                    $time_logon = substr($time_logon, 0, -1);
+                    $time_logon = str_replace('T',' ',$time_logon);
+                    $time_logon = explode(".",$time_logon);
+                    $time_logon = $time_logon[0];
+                    $time_logon = Carbon::createFromFormat('Y-m-d H:i:s', $time_logon)->timestamp;
 					$time_now = strtotime(Carbon::now());
 					$duration = $time_now - $time_logon;
 
@@ -137,51 +125,19 @@ class OnlineControllerUpdate extends Command
 						$MostRecentLog->save();
 					}
 				}
-			}
+			
 
-			if ($line != "!CLIENTS:")
-				continue;
-			else
-				$client_section = true;
+			
 		}
 		$time = Carbon::now('Etc/UTC')->format('H:i').'Z';
     }
 
     public function getStatsData() {
-        $statusData = file_get_contents($this->statusUrl);
-		$n = preg_match_all("/^url0=(.*)$/m", $statusData, $matches);
-		$urls = $matches[1];
-		shuffle($urls);
-		$data = false;
+        $client = new Client();
+        $res = $client->request('GET', $this->statusUrl);
 
-		foreach($urls as $url) {
-			$data_file = file(trim($url));
+        $data = $res->getBody();
 
-			foreach ($data_file as $record) {
-				if (substr($record, 0, 9) == 'UPDATE = ') {
-					$streamupdate = rtrim(substr($record, 9));
-					$update_time = gmmktime(
-						substr($streamupdate,8,2),
-						substr($streamupdate,10,2),
-						substr($streamupdate,12,2),
-						substr($streamupdate,4,2),
-						substr($streamupdate,6,2),
-						substr($streamupdate,0,4)
-					);
-					break;
-				}
-			}
-
-			$age = time() - $update_time;
-			if ($age < 600) {
-				$data = $data_file;
-				break;
-			}
-		}
-
-		if (!$data) {
-			throw \Exception("No data source found that is younger than 10 minutes old.");
-		}
 
 		return $data;
     }
