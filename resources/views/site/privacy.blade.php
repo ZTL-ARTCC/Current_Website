0e65af912171fc2125a946eec6282ac35b2abd05
TheRealJBomb <43980009+TheRealJBomb@users.noreply.github.com>
2021-06-25 21:25:54 -0700

    Update privacy.blade.php

--- resources/views/site/privacy.blade.php
+++ resources/views/site/privacy.blade.php
@@ -1,45 +1,113 @@
 @extends('layouts.master')
 
 @section('title')
-Teamspeak
+Privacy
 @endsection
 
 @section('content')
 <span class="border border-light" style="background-color:#F0F0F0">
     <div class="container">
         &nbsp;
-        <h2>ZTL ARTCC Teamspeak Information</h2>
+        <h2>ZTL ARTCC Privacy Policy/Terms and Conditions</h2>
         &nbsp;
     </div>
 </span>
-<br>
-
-<div class="container">
-    <center><h1><a href="https://www.teamspeak.com/?tour=yes" target="_blank"><img width="50" src="/photos/ts_stacked.png"></a> vZTL ARTCC Teamspeak Information <a href="https://www.teamspeak.com/?tour=yes" target="_blank"><img width="50" src="/photos/ts_stacked.png"></a></h1></center>
-    <center><p><i>Thank you to TeamSpeak for providing us with our TeamSpeak license. Check them out at <a href="https://www.teamspeak.com/?tour=yes" target="_blank">https://www.teamspeak.com</a> for more information!</i></p></center>
-    <hr>
-    <center><h3>Teamspeak is used for all training and all controller communications, but all members are welcome to join us in the teamspeak. You can connect to the teamspeak server, without a password, using the IP:</h3></center>
-    <center><h2><a href="ts3server://ts.ztlartcc.org?port=9987">ts.ztlartcc.org</a></h2></center>
-    <hr>
-    <center><h2>Teamspeak Rules:</h2></center>
-    <div class="row">
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-8">
-            <ul>
-                <li>All users must join the teamspeak using the name associated with their VATSIM account.</li>
-                <li>Anonymous users will be kicked with a warning and banned upon reconnecting anonymously.</li>
-                <li>Teamspeak permissions are required to move within the teamspeak. Please contact a staff member to receive appropiate permissions. (If you are not a member of the VATUSA region, please provide your CID for rating verification).</li>
-                <li>Streaming while controlling is allowed and encouraged although the audio from the teamspeak is not allowed for the privacy of other controllers. Streaming teamspeak audio requires the written permission of the ATM (Contact the ATM at <a href="atm@ztlartcc.org">atm@ztlartcc.org</a> for permission). Streaming audio without his/her permission may result in loss of teamspeak privileges.</li>
-                <li>Controlling rooms are limited to controlling only. If a controller asks for you to leave, please do so.</li>
-                <li>Use of the teamspeak is a privilage and can be revoked by a staff member at any time and for any reason. To appeal teamspeak bans, please contact the DATM at <a href="datm@ztlartcc.org">datm@ztlartcc.org</a>.</li>
-            </ul>
-        </div>
-        <div class="col-sm-2">
-        </div>
-    </div>
-    <hr>
-    <center><p>If you have any additional questions, please contact either the ATM or DATM at <a href="atm@ztlartcc.org">atm@ztlartcc.org</a> or <a href="datm@ztlartcc.org">datm@ztlartcc.org</a> respectively.</p></center>
+<div class="react-reveal">
+<div class="container-fluid">
+<div class="mb-4">
+<h2><em>Information We Collect</em></h2>
+<p>To help enhance the Virtual Atlanta ARTCC experience, we collect information across a series of products and services all to enhance the end user's experience. We collect information in the following ways:</p>
+<ul>
+<li><strong>Information you give us.</strong>&nbsp;When you register an account with VATSIM and transfer to Virtual Atlanta ARTCC, login to the Virtual Atlanta ARTCC service, take a quiz, use our Computer Based Training (CBT) system, or participate in the forums, information that is generally considered personal is given to us from you and VATSIM to include, but is not limited to: your name and email address.</li>
+<li><strong>Information we get from you.</strong>&nbsp;Some other information is passed by your computer or electronic device, web browser, and VATSIM client. Information can include: your IP address, web browser type and version, device specific information (such as operating system, unique device identifiers and mobile network information). This information may be linked to your account.</li>
+<li><strong>Log Information.</strong>&nbsp;Each time you perform an action on Virtual Atlanta ARTCC services, your action is logged. Information logged can include: type of action, data being sent and received, IP address the request originated from, software used to make the request, identification cookies, and the results of the request.</li>
+<li><strong>Location Information.</strong>&nbsp;Your location information may be associated with each request through geolocation against the originating IP address, information given to us by you or given to us by you through VATSIM.</li>
+<li><strong>Analytical Information.</strong>&nbsp;Via Google Analytics, we may collect anonymized statistical information to include: age, gender, location, device information, web browser type and version, ISPs, mobile network provider information, etc. for the purposes of identifying trends and better target our platforms.</li>
+<li><strong>Storage Location.</strong>&nbsp;Data is stored and encrypted on services owned or leased by Virtual Atlanta ARTCC within the United States.</li>
+</ul>
 </div>
+<div class="mb-4">
+<h2><em>Information Usage</em></h2>
+<p>To help enhance the Virtual Atlanta ARTCC experience, we collect information across a series of products and services all to enhance the user experience.</p>
+<p>We use the information collected to provide, maintain, protect and improve our services.</p>
+<p>The information we collect is maintained with confidentiality to the extent possible. The following information is shared with Virtual Atlanta ARTCC associated facilities:</p>
+<ul>
+<li>VATSIM CERT Identification Number (CID)</li>
+<li>Your name</li>
+<li>Your VATSIM achievements and ratings</li>
+<li>Your VATSIM associated email address</li>
+<li>Virtual Atlanta ARTCC facility associations, VATSIM region and division associations</li>
+<li>Virtual Atlanta ARTCC staff associations and Virtual Atlanta ARTCC staff email addresses</li>
+</ul>
+<p>The following information may be shared publicly:</p>
+<ul>
+<li>VATSIM CERT Identification Number (CID)</li>
+<li>Your name</li>
+<li>Virtual Atlanta ARTCC staff associations and Virtual Atlanta ARTCC staff email addresses</li>
+<li>Virtual Atlanta ARTCC facility associations, VATSIM region and division associations</li>
+<li>VATSIM achievements and ratings</li>
+</ul>
+<p>The following information is collected and may be used to protect our services, up to and including cooperation with legal requests for information from Law Enforcement agencies:</p>
+<ul>
+<li>The above listed public information,</li>
+<li>All IP addresses used and associated with your account</li>
+<li>Geolocation against aforementioned IP addresses</li>
+<li>Activities performed with the Virtual Atlanta ARTCC web services</li>
+</ul>
+<p>We may store identification tokens and other limited information on your electronic device through web storage or cookie usage.</p>
+</div>
+<div class="mb-4">
+<h2><em>Who We Share With</em></h2>
+<p>The information we collect may be shared, in limited capacities, with the following:</p>
+<ul>
+<li>Virtual Air Traffic Simulation Network (<a href="https://vatsim.net/">www.vatsim.net</a>)</li>
+<li>VATUSA (<a href="https://vatusa.net/">www.vatusa.net</a>)</li>
+<li>Other Virtual Atlanta ARTCC partners</li>
+<li>Law Enforcement agencies</li>
+<li>Google Analytics</li>
+</ul>
+<p>For more information on what is shared with whom, please see "How We Use Information".</p>
+</div>
+<div class="mb-4">
+<h2><em>Cookie Usage</em></h2>
+<p>We use various technologies to collect and store information when you visit and use a Virtual Atlanta ARTCC service. This may include a cookie or other similar technologies to identify your browser or device. We also use Google Analytics to help analyze the traffic to our websites and the information may be linked, by Google Analytics, with customers for information across multiple websites.</p>
+<p>Our cookies are mainly used as a means of tracking virtual users across the Virtual Atlanta ARTCC domain. This allows us to know who is requesting and using our services, provide authentication and authorization checks to restricted areas.</p>
+<p>You may choose to disable cookie usage via your browser, but know that doing so will prevent access and use across restricted areas of the website and severely degrade your experience.</p>
+</div>
+<div class="mb-4">
+<h2><em>Opt Out</em></h2>
+<p>Given the nature of our services, it is not possible to opt out of data collection and use our services. But if you desire to opt out and no longer desire to use our services, we will purge all information we have collected upon written request.</p>
+<p>The first step is to deactivate and request VATSIM to purge your data. Please head to&nbsp;<a href="https://membership.vatsim.net/">https://membership.vatsim.net</a>&nbsp;to do so.</p>
+<p>After VATSIM has purged your data, please send a written request to Virtual Atlanta ARTCC's Air Traffic Manager by emailing <a href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=atm@zhuartcc.org" target="_blank" rel="noopener">atm@ztlartcc.org</a>. It is a manual process, so please allow up to 30 days for information to be purged. You will receive an email response once the data has been purged.</p>
+<p>Note Virtual Atlanta ARTCC cannot guarantee that information collected by parties outside of Virtual Atlanta ARTCC will be purged in the process.</p>
+</div>
+</div>
+</div>
+<footer>
+<div class="d-flex justify-content-center mb-5">
+<div class="position-unset col-xl-6 col-10">
+<div class="fade position-unset m-0 undefined alert alert-primary show" role="alert">
+<table>
+<tbody>
+<tr>
+<td class="pb-2 pb-md-1">&nbsp;</td>
+<td class="pb-2 pb-md-1">
+<h5 class="mb-0">Disclaimer!</h5>
+</td>
+</tr>
+<tr>
+<td class="d-none d-md-table">&nbsp;</td>
+<td colspan="2">
+<p class="m-0">All information on this website is for flight simulation use only and is not to be used for real world navigation or flight. This site is not affiliated with ICAO, the FAA, the actual Atlanta ARTCC, or any other real world aerospace entity.</p>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</footer>
+
+
 
 @endsection
