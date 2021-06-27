43bd1097c36ae956bbe33fbfeedd43f13841b7fc
TheRealJBomb <43980009+TheRealJBomb@users.noreply.github.com>
2021-06-26 19:44:39 -0700

    Update new.blade.php
    changed visitor email as per the datm's request (new exam they must take).

--- resources/views/emails/visit/new.blade.php
+++ resources/views/emails/visit/new.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 <p>Dear {{ $visit->name }},</p>
 
-<p>Your visitor application has been successfully submitted. You will be assigned a ZTL Visitor Exam that must be passed prior to your application being considered by our executive staff. This exam is more of a getting familiar with our Standard Operating Procedures. You will have seven (7) days to complete the exam and you must pass with a score of 80% or better. If you have any questions or concerns, please email the DATM at <a href="mailto:datm@ztlartcc.org">datm@ztlartcc.org</a>.</p>
+<p>Your visitor application has been successfully submitted. In the next 24 hours, you will be assigned two exams via the VATUSA Exam Center. The first part of the GRP check will ensure you have the skills required to control at ZTL. Some questions will require you to reference various SOPs and LOAs that can be found on the <a href="www.ztlartcc.org/dashboard/controllers/files">ZTL files page.</a></p>
+<p>The second part of the GRP check will ensure you understand the rules for being a member at ZTL. The answers to these questions can be found in our Facility Administrative Policy on the files page.</p>
+<p>You will have seven (7) days to complete the exams and you must pass each with a score of 80% or better. If you have any questions or concerns, please email the DATM at <a href="mailto:datm@ztlartcc.org">datm@ztlartcc.org</a>.</p>
 
 <p>Best regards,</p>
 <p>ZTL Visiting Staff</p>
