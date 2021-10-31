a97aba01b0b5e16670d1d5334c3e23bdc0d00cbb
kjporter <kporter@kplink.net>
2021-10-30 21:35:33 -0700

    Update footer.blade.php
    Updated partner links

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,6 +1,7 @@
 <footer id="myFooter">
     <div class="footer-social">
-        <a href="http://www.vatstar.com/" target="_blank"><img width="200" src="/photos/vatstar.png"></a> &nbsp;&nbsp;&nbsp; <a style="position: relative; top: -10px" href="https://www.teamspeak.com/?tour=yes" target="_blank"><img width="200" src="/photos/ts_inline.png"></a> &nbsp;&nbsp;&nbsp; <a style="position: relative; top: -10px" href="https://www.thepilotclub.org/" target="_blank"><img height="100" src="/photos/tpc-logo.png"></a>
+        <p>Visit our partners:</p>
+        <a href="http://www.vatstar.com/" target="_blank"><img width="200" src="/photos/partner-vatstar.png"></a> &nbsp;&nbsp;&nbsp; <a style="position: relative; top: -10px" href="https://www.teamspeak.com/?tour=yes" target="_blank"><img width="200" src="/photos/partner-ts.png"></a> &nbsp;&nbsp;&nbsp; <a style="position: relative; top: -10px" href="https://www.thepilotclub.org/" target="_blank"><img height="100" src="/photos/partner-tpc.png"></a>
         <br><br>
         <p><i>For entertainment purposes only. Do not use for real world purposes. Part of the VATSIM Network.</i></p>
         @if(Carbon\Carbon::now()->month == 12)
