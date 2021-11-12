d41ff00599313c5383d99a901e69bc489c03fa4e
kjporter <kporter@kplink.net>
2021-11-12 13:48:44 -0700

    Update AdminDash.php
    Fixes roster update error when user does not have an email address on file

--- app/Http/Controllers/AdminDash.php
+++ app/Http/Controllers/AdminDash.php
@@ -831,12 +831,12 @@ class AdminDash extends Controller
             $audit->ip = $_SERVER['REMOTE_ADDR'];
             $audit->what = Auth::user()->full_name.' removed the visitor '.$name.'.';
             $audit->save();
-            
+            if(filter_var($user->email, FILTER_VALIDATE_EMAIL)) { // Added this to deal with case when user does not have an email address on file
              Mail::send('emails.remove_visitor', ['user' => $user], function($message) use ($user){
                         $message->from('datm@ztlartcc.org', 'vZTL ARTCC Staff')->subject('Notification of ZTL Roster Removal');
                         $message->to($user->email)->cc('datm@ztlartcc.org');
              });
-
+			}
             return redirect('/dashboard/controllers/roster')->with('success', 'The visitor has been removed successfully.');
         }
     }
