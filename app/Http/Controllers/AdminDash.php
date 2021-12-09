05365328be81beb1bdeae7e1312902747c408b69
Kyle <kporter@kplink.net>
2021-12-09 16:00:17 -0700

    Update AdminDash.php

--- app/Http/Controllers/AdminDash.php
+++ app/Http/Controllers/AdminDash.php
@@ -1039,7 +1039,7 @@ class AdminDash extends Controller
 		}
 		// Rebuild the display and return it to the AJAX caller
 		$dispString = "";
-		//$files = File::where('type', $request->typ)->get();
+		$files = File::where('type', $request->typ)->get();
 		/*
 		foreach($files as $f) {
 			$dispString .= "<tr>
