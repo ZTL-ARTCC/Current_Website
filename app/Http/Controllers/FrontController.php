da30facdb1fed11cff0ea1491ac36161f788136d
Kyle <kporter@kplink.net>
2021-12-19 20:32:14 -0700

    Ramp view improvements

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -440,9 +440,15 @@ class FrontController extends Controller
 	
 	public function showAtlRamp() {
         return view('site.atl-ramp');
+//        return view('site.ramp')->with('afld','ATL');
     }
 	
 	public function showCltRamp() {
         return view('site.clt-ramp');
+//        return view('site.ramp')->with('afld','CLT');
+    }
+	
+	public function showTestRamp() {
+        return view('site.ramp')->with('afld','ATL');
     }
 }
