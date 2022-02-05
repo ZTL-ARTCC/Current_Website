cc83dabf461561fda5e831953d248e03ad6f19a3
Kyle <kporter@kplink.net>
2022-02-05 09:33:40 -0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+	// Added for Laravel 8 migration compatibility
+	protected $table='roster';
+
     /**
      * The attributes that are mass assignable.
      *
