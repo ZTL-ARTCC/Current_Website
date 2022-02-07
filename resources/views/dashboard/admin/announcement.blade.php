df2ac776c22858e9b82c68264c3e757125f1f9ab
Kyle <kporter@kplink.net>
2022-02-07 13:46:45 -0700

    ckeditor package install
    Install new ckeditor package

--- resources/views/dashboard/admin/announcement.blade.php
+++ resources/views/dashboard/admin/announcement.blade.php
@@ -23,7 +23,8 @@ Announcement
     {!! Form::close() !!}
 </div>
 
-<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+<!--<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>-->
+<script src={{ asset('ckeditor/ckeditor.js')}}></script>
 <script>
     CKEDITOR.replace( 'article-ckeditor' );
 </script>
