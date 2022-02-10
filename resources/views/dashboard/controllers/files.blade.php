835b325b98476e70283dd060f1ceca52aa163e18
Kyle <kporter@kplink.net>
2022-02-10 12:38:27 -0700

    Update files.blade.php
    Bugfix

--- resources/views/dashboard/controllers/files.blade.php
+++ resources/views/dashboard/controllers/files.blade.php
@@ -67,7 +67,6 @@ Files
                                     @if(Auth::user()->isAbleTo('files'))
                                         <a href="/dashboard/admin/files/edit/{{ $f->id }}" class="btn btn-warning simple-tooltip" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
                                         <a href="/dashboard/admin/files/delete/{{ $f->id }}" onclick="return confirm('Are you sure you want to delete {{ $f->name }}?')" class="btn btn-danger simple-tooltip" data-toggle="tooltip" title="Delete"><i class="fas fa-times"></i></a>
-										<a href="/dashboard/files/permalink/{{ $f->id }}" class="btn btn-secondary simple-tooltip disabled" data-toggle="tooltip" title="Permalink"><i class="fa-solid fa-link"></i></a>
 										@if(!$loop->first)
 											<a onclick="itemReorder({{ $f->id }},{{ $loop->index }},{{ $f->type }},'up');" class="btn btn-info simple-tooltip" data-toggle="tooltip" title="Up"><i class="fas fa-arrow-up"></i></a>
 										@endif
@@ -220,6 +219,7 @@ Files
                                     @if(Auth::user()->isAbleTo('files'))
                                         <a href="/dashboard/admin/files/edit/{{ $f->id }}" class="btn btn-warning simple-tooltip" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
                                         <a href="/dashboard/admin/files/delete/{{ $f->id }}" onclick="return confirm('Are you sure you want to delete {{ $f->name }}?')" class="btn btn-danger simple-tooltip" data-toggle="tooltip" title="Delete"><i class="fas fa-times"></i></a>
+										<a href="/dashboard/files/permalink/{{ $f->id }}" class="btn btn-secondary simple-tooltip disabled" data-toggle="tooltip" title="Permalink"><i class="fas fa-link"></i></a>
 										@if(!$loop->first)
 											<a onclick="itemReorder({{ $f->id }},{{ $loop->index }},{{ $f->type }},'up');" class="btn btn-info simple-tooltip" data-toggle="tooltip" title="Up"><i class="fas fa-arrow-up"></i></a>
 										@endif
@@ -258,6 +258,7 @@ Files
                                     @if(Auth::user()->isAbleTo('files'))
                                         <a href="/dashboard/admin/files/edit/{{ $f->id }}" class="btn btn-warning simple-tooltip" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
                                         <a href="/dashboard/admin/files/delete/{{ $f->id }}" onclick="return confirm('Are you sure you want to delete {{ $f->name }}?')" class="btn btn-danger simple-tooltip" data-toggle="tooltip" title="Delete"><i class="fas fa-times"></i></a>
+										<a href="/dashboard/files/permalink/{{ $f->id }}" class="btn btn-secondary simple-tooltip disabled" data-toggle="tooltip" title="Permalink"><i class="fas fa-link"></i></a>
 										@if(!$loop->first)
 											<a onclick="itemReorder({{ $f->id }},{{ $loop->index }},{{ $f->type }},'up');" class="btn btn-info simple-tooltip" data-toggle="tooltip" title="Up"><i class="fas fa-arrow-up"></i></a>
 										@endif
