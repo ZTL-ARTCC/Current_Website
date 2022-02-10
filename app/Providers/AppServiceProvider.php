e3e67de6d845796a9435ccc3ad8d877709c1c9c3
Kyle <kporter@kplink.net>
2022-02-10 07:02:48 -0700

    Fix pagination bug
    Force bootstrap pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-//use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-		//Paginator::useBootstrap();
+		Paginator::useBootstrap();
     }
 
     /**
