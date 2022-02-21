12c9047d8d94c295f5cade6338b153f8ba326ca3
kjporter <kporter@kplink.net>
2022-02-20 21:34:01 -0700

    Update files.blade.php

--- resources/views/dashboard/controllers/files.blade.php
+++ resources/views/dashboard/controllers/files.blade.php
@@ -371,8 +371,23 @@ Files
 	}
 	
 	function linkToClipboard(e) {
-		var path = location.pathname + e.getAttribute('title');
+		var path = getSiteRoot() + e.getAttribute('title');
 		copyTextToClipboard(path);
+	}
+	
+	function getSiteRoot() {
+		var rootPath = window.location.protocol + "//" + window.location.host + "/";
+		if (window.location.hostname == "localhost") {
+			var path = window.location.pathname;
+			if (path.indexOf("/") == 0) {
+				path = path.substring(1);
+			}
+			path = path.split("/", 1);
+			if (path != "") {
+				rootPath = rootPath + path + "/";
+			}
+		}
+		return rootPath;
 	}
 		</script>
     </div>
