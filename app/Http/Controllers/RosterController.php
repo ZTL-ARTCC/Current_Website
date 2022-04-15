42c067d37b77ca45c6c2014f3d9e89b856e40a6c
kjporter <kporter@kplink.net>
2022-04-15 06:48:06 -0700

    Update RosterController.php
    Fixed roster bug

--- app/Http/Controllers/RosterController.php
+++ app/Http/Controllers/RosterController.php
@@ -20,9 +20,14 @@ use Session;
 class RosterController extends Controller
 {
     public function index() {
-        $hcontrollers = User::where('visitor', '0')->where('status', '1')->orderBy('lname', 'ASC')->get();
-        $vcontrollers = User::where('visitor', '1')->where('status', '1')->where('visitor_from', '!=', 'ZJX')->orderBy('lname', 'ASC')->get();
-        $visagreecontrollers = User::where('visitor', '1')->where('visitor_from', 'ZJX')->orderBy('visitor_from', 'ASC')->orderBy('lname', 'ASC')->get();
+        $hcontrollers = User::where('status', '!=', 2)->where('visitor', '0')->where('status', '1')->orWhere('status', '0')->orderBy('lname', 'ASC')->get();
+        $vcontrollers = User::where('status', '!=', 2)->where('visitor', '1')->where('status', '1')->orderBy('lname', 'ASC')->get();
+ 
+        return view('dashboard.controllers.roster')->with('hcontrollers', $hcontrollers)->with('vcontrollers', $vcontrollers);
+		
+//        $hcontrollers = User::where('visitor', '0')->where('status', '1')->orderBy('lname', 'ASC')->get();
+//        $vcontrollers = User::where('visitor', '1')->where('status', '1')->where('visitor_from', '!=', 'ZJX')->orderBy('lname', 'ASC')->get();
+//        $visagreecontrollers = User::where('visitor', '1')->where('visitor_from', 'ZJX')->orderBy('visitor_from', 'ASC')->orderBy('lname', 'ASC')->get();
 
         return view('site.roster')->with('hcontrollers', $hcontrollers)->with('vcontrollers', $vcontrollers)->with('visagreecontrollers', $visagreecontrollers);
     }
