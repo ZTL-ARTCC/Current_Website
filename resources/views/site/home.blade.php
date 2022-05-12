ca4d9a0d07fd87084e41c736fb3d37d5a0d4c9b1
Kyle <kporter@kplink.net>
2022-05-12 10:44:50 -0700

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -25,7 +25,7 @@ Home
             @if($events->count() > 0)
                 @foreach($events as $e)
 			<div class="carousel-item @if ($loop->first) active @endif">
-                    <a href="/dashboard/controllers/events/view/{{ $e->id }}"><img src="{{ $e->banner_path }}" class="d-block w-100" alt="{{ $e->name }}" /></a>
+                    <a href="/dashboard/controllers/events/view/{{ $e->id }}"><img src="{{ $e->banner_path }}" class="d-block w-100 rounded" alt="{{ $e->name }}" /></a>
             </div>
                 @endforeach
             @else
