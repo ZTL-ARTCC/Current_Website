a4be52999ea62346c73fd3a057c444459334cc2c
Kyle <kporter@kplink.net>
2022-05-15 15:17:20 -0700

    Update navbar.blade.php

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,5 +1,4 @@
-<div class="container-fluid bg-dark">
-<div class="row"><div class="col">
+<div class="container-fluid bg-dark p-0">
     <nav class="navbar navbar-expand-lg navbar-dark">
         <a class="navbar-brand" href="/"><img width="100" src="/photos/logo.png"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
@@ -58,4 +57,4 @@
             </ul>
         </div>
     </nav>
-</div></div></div>
+</div>
