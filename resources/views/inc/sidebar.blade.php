0a9d2220955383eec9f32556c110fe1000c1178b
Kyle <kporter@kplink.net>
2022-05-21 14:38:37 -0700

    Update sidebar.blade.php

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -163,12 +163,12 @@
 <?php // @include('inc.chat') // Not using this anymore - functionality replaced with Discord ?>
 @if(isset($home))
 <div class="card">
-	<div class="card-body p-0">
+	<div class="card-body p-2">
 		<h5 class="card-title">{{ Carbon\Carbon::now()->translatedFormat('F') }} Leaders&nbsp;<i class="fas fa-medal"></i></h5>
 		<ul class="list-group list-group-flush">
 		@if(count($home) > 0)
             @foreach($home as $h)
-				<li class="list-group-item d-flex px-1"><strong>{{ $h->full_name }}</strong>&nbsp;{{ $stats[$h->id]->bronze_hrs }} hrs</li>
+				<li class="list-group-item d-flex px-1"><strong>{{ $h->full_name }}</strong>&nbsp;{{ $stats[$h->id]->bronze_hrs }}</li>
             @endforeach
         @else
             <li class="list-group-item d-flex"><i>So empty...</i></li>
@@ -182,7 +182,7 @@
 		<ul class="list-group list-group-flush">
         @if($controllers->count() > 0)
             @foreach($controllers as $c)
-				<li class="list-group-item d-flex"><strong>{{ $c->name }}</strong>&nbsp;{{ $c->time_online }}</li>
+				<li class="list-group-item d-flex px-1"><strong>{{ $c->name }}</strong>&nbsp;{{ $c->time_online }}</li>
             @endforeach
         @else
             <li class="list-group-item d-flex"><i>So empty...</i></li>
