d3f1c668f7a278a5cf2e24c512e9d1f987ed4ac3
Kyle <kporter@kplink.net>
2022-05-22 12:21:56 -0700

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -56,7 +56,7 @@ $('.carousel').carousel({
 	transform:scale(1.1);
 	background-image:url("/photos/asx_bg.png");
 	background-repeat: no-repeat;
-	background-size: auto;
+	background-size: contain;
 	background-color:red;
 }
 </style>
@@ -236,7 +236,7 @@ $('.carousel').carousel({
  </div>
  <!-- Modal -->
 <div class="modal fade" id="displayAsx" tabindex="-1" role="dialog" aria-labelledby="displayAsx" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">ZTL Airspace Explorer</h5>
@@ -245,11 +245,11 @@ $('.carousel').carousel({
         </button>
       </div>
       <div class="modal-body">
-        <embed src="https://asx.kporter.dev" frameborder="0" style="width:90%; height:70vh" >
+        <embed src="https://asx.kporter.dev#dispNode" frameborder="0" style="height:70vh" >
       </div>
-      <div class="modal-footer">
+<!--      <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
+      </div> -->
     </div>
   </div>
 </div>
