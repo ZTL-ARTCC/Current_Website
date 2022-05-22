2b4447a204357204ca856d530ade3ff544b0b1c0
Kyle <kporter@kplink.net>
2022-05-22 12:38:30 -0700

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -237,7 +237,7 @@ $('.carousel').carousel({
  <!-- Modal -->
 <div class="modal fade" id="displayAsx" tabindex="-1" role="dialog" aria-labelledby="displayAsx" aria-hidden="true">
   <div class="modal-dialog" style="height:80vh; max-width:90vw" role="document">
-    <div class="modal-content bg-secondary">
+    <div class="modal-content"> <!--bg-secondary-->
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">ZTL Airspace Explorer</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
