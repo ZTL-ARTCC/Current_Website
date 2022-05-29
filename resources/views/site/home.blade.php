d13a46bffcc314e618de74978a9dcb1bb02ae973
Kyle <kporter@kplink.net>
2022-05-29 10:12:44 -0700

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -236,7 +236,7 @@ $('.carousel').carousel({
  </div>
  <!-- Modal -->
 <div class="modal fade" id="displayAsx" tabindex="-1" role="dialog" aria-labelledby="displayAsx" aria-hidden="true">
-  <div class="modal-dialog" style="height:80vh; max-width:90vw" role="document">
+  <div class="modal-dialog bg-secondary py-1" style="height:90vh; max-width:90vw" role="document">
     <div class="modal-content"> <!--bg-secondary-->
       <!--<div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">ZTL Airspace Explorer</h5>
