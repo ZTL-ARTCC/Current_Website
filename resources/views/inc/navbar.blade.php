36f9db6818213d12ad408d0ad7840572c46b9235
kjporter <kporter@kplink.net>
2022-05-30 10:40:43 -0700

    Update navbar.blade.php

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,11 +1,11 @@
 <div class="bg-dark w-100">
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-dark">
-        <a class="navbar-brand" href="/"><img width="100" src="/photos/logo.png"></a>
+        <a class="navbar-brand" href="/"><img width="100" src="/photos/ztl_logo_white.png"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
+		<style>a.nav-link{ color: white !important }</style>
         <div class="collapse navbar-collapse" id="navbarsExample10">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
