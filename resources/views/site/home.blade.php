d040de6b61e251271ef27f3354e45ba3aea756b9
Kyle <kporter@kplink.net>
2022-05-31 22:46:59 -0700

    Update home.blade.php
    Fixed carousel buttons

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -34,11 +34,11 @@ Home
 			</div>
             @endif
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#eventCarousel" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#eventCarousel" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
