ceb179e6b6a6733ce373ba81c0d49340c304fcc5
Ian Cowan <ian@cowan.aero>
2022-09-08 22:43:31 -0400

    #171 Regenerate assets, remove vue example

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,25 +8,3 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
-
-// Add slideDown animation to dropdown
-$('.dropdown').on('show.bs.dropdown', function(e){
-  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
-});
-
-// Add slideUp animation to dropdown
-$('.dropdown').on('hide.bs.dropdown', function(e){
-  $(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
-});
