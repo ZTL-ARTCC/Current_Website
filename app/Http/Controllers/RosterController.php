a47cf22662a2d1b08e36c1c5ae946035e1856342
Ian Cowan <ian@cowan.aero>
2022-09-13 21:50:20 -0400

    NOISS Allow local configuration of vatusa return url, update example env

--- app/Http/Controllers/RosterController.php
+++ app/Http/Controllers/RosterController.php
@@ -51,7 +51,7 @@ class RosterController extends Controller
         } else {
             if(!Auth::check() && !isset($_GET['token'])) {
                 $_SESSION['redirect'] = Config::get('app.url');
-                header("Location: https://login.vatusa.net/uls/v2/login?fac=".Config::get('vatusa.facility')."&dev=1&url=3");
+                header("Location: https://login.vatusa.net/uls/v2/login?fac=".Config::get('vatusa.facility')."&dev=1&url=".Config::get('vatusa.dev_callback'));
                 exit;
             }
         }
--- config/vatusa.php
+++ config/vatusa.php
@@ -5,5 +5,6 @@ return [
     'facility' => env('VATUSA_FACILITY'),
     'jwk' => env('VATUSA_ULS_JWK'),
     'api_key' => env('VATUSA_API_KEY'),
+    'dev_callback' => env('VATUSA_DEV_CALLBACK_URL', '3'),
 
 ];
