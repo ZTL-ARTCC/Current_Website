1617f3afc227b8eb3a04cdae709e58432281ce99
Ian Cowan <ian@cowanemail.com>
2022-10-05 08:03:57 -0400

    #186 Add php auto formatting tool php-cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,24 @@
+<?php
+
+$finder = Symfony\Component\Finder\Finder::create()
+    ->notPath('vendor')
+    ->notPath('bootstrap')
+    ->notPath('storage')
+    ->in(__DIR__)
+    ->name('*.php')
+    ->notName('*.blade.php');
+
+$config = new PhpCsFixer\Config();
+
+$config->setRules([
+    '@PSR2' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'ordered_imports' => ['sort_algorithm' => 'alpha'],
+    'no_unused_imports' => true,
+    'braces' => [
+        'position_after_functions_and_oop_constructs' => 'same',
+        'allow_single_line_closure' => true,
+    ],
+])->setFinder($finder);
+
+return $config;
