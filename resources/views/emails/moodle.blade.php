3723bfdff80dd563d4bfe764e18f7d492a2d5cd8
Ian Cowan <ian@cowanemail.com>
2022-10-26 21:49:41 -0400

    #193 Remove moodle views and email templates

--- resources/views/emails/moodle.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.email')
-
-@section('content')
-<p>Please add user past and up to current ratings correct enrolment on Moodle</p>
-<ul>
-  <li><b>Controller Name:</b> {{user->fname}} {{user->lname}}</li></b>
-  <li><b>Controller Email:</b< {{user->email}}<li></b><li>
-  <li><b>Controller Rating:</b> {{user->rating_id}}</b></li>
- </ul>
