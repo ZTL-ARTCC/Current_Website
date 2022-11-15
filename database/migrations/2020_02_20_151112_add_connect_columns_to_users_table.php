c21407cb86f249c5056032c4d9a4d4b68ba3294d
kjporter <kporter@kplink.net>
2022-11-14 22:01:13 -0700

    Updated formatting
    Updated code formatting

--- database/migrations/2020_02_20_151112_add_connect_columns_to_users_table.php
+++ database/migrations/2020_02_20_151112_add_connect_columns_to_users_table.php
@@ -4,16 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddConnectColumnsToUsersTable extends Migration
-{
+class AddConnectColumnsToUsersTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::table('roster', function (Blueprint $table) {            
+    public function up() {
+        Schema::table('roster', function (Blueprint $table) {
             $table->text('access_token')->nullable();
             $table->text('refresh_token')->nullable();
             $table->unsignedBigInteger('token_expires')->nullable();
@@ -25,15 +23,14 @@ class AddConnectColumnsToUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        $columns = array('token_expires','refresh_token','access_token');
-        foreach($columns as $column) {
-        if (Schema::hasColumn('roster', $column)) {
-            Schema::table('roster', function (Blueprint $table) {
-                $table->dropColumn($column);
-            });
+    public function down() {
+        $columns = ['token_expires', 'refresh_token', 'access_token'];
+        foreach ($columns as $column) {
+            if (Schema::hasColumn('roster', $column)) {
+                Schema::table('roster', function (Blueprint $table) {
+                    $table->dropColumn($column);
+                });
+            }
         }
     }
-
 }
