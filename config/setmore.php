b4ecdda88974b54457940b461d1dd2c19e35a10c
kjporter <kporter@kplink.net>
2022-12-04 20:10:23 -0700

    Add environment & config
    Adds setmore and ZTL TA config keys

---
+++ config/setmore.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'endpoint' => env('SETMORE_API_ENDPOINT', 'https://developer.setmore.com/api/v1'),
+    'api_key' => env('SETMORE_API_KEY'),
+
+];
---
+++ config/ztl.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'trainer_min_sessions' => env('ZTL_TRAINER_MONTHLY_SESSIONS', 3),
+];
