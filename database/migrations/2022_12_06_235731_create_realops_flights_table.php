16a9a6ba383b710c0f108360043d34a888c0818e
Ian Cowan <ian@cowanemail.com>
2022-12-09 19:51:37 -0500

    #221 Add migrations for realops

---
+++ database/migrations/2022_12_06_235731_create_realops_flights_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('realops_flights', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assigned_pilot_id')->nullable()->references('id')->on('realops_pilots')->nullOnDelete();
+            $table->string('flight_number');
+            $table->date('flight_date');
+            $table->time('dep_time');
+            $table->string('dep_airport');
+            $table->string('arr_airport');
+            $table->text('route')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('realops_flights');
+    }
+};
