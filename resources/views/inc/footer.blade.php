cbc39759d4bb18e2a0d7f61bf6ff078b681e28db
kjporter <kporter@kplink.net>
2022-12-14 09:51:06 -0700

    Adjust stop snow button visibility to pages with snow

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -4,9 +4,6 @@
         <a href="http://www.vatstar.com/" target="_blank"><img width="200" src="/photos/partner-vatstar.png"></a> &nbsp;&nbsp;&nbsp; <a style="position: relative; top: -10px" href="https://www.teamspeak.com/?tour=yes" target="_blank"><img width="200" src="/photos/partner-ts.png"></a> &nbsp;&nbsp;&nbsp; <a style="position: relative; top: -10px" href="https://www.thepilotclub.org/" target="_blank"><img height="100" src="/photos/partner-tpc.png"></a>
         <br><br>
         <p><i>For entertainment purposes only. Do not use for real world purposes. Part of the VATSIM Network.</i></p>
-        @if(Carbon\Carbon::now()->month == 12)
-            <button class="btn btn-secondary btn-sm" onclick="snowStorm.stop();return false">Stop Snow</button>
-        @endif
     </div>
     <div class="container">
         <p><a href="https://www.ztlartcc.org/privacy" target="_blank">Privacy Policy, Terms and Conditions</a></p>
