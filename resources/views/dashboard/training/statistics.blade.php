75ac1658ee62b36581ef4b9a90593a5a018a0e1b
kjporter <kporter@kplink.net>
2022-12-14 08:45:46 -0700

    Fixes blade styles
    Fixes blade styles by adding margins between cards, tooltip, and hover effects

--- resources/views/dashboard/training/statistics.blade.php
+++ resources/views/dashboard/training/statistics.blade.php
@@ -4,6 +4,10 @@
 Training Statistics
 @endsection
 
+@push('custom_header')
+<link rel="stylesheet" href="{{ asset('css/clipboard.css') }}" />
+@endpush
+
 @section('content')
 <div class="container-fluid" style="background-color:#F0F0F0;">
     &nbsp;
@@ -24,31 +28,31 @@ Training Statistics
                 {!! Form::select('date_select', $yearOfMonthsLookback, implode(' ', $stats['dateSelect']), ['class' => 'form-control', 'onchange'=>"this.form.submit();"]) !!}
                 {!! Form::close() !!}
             </div>
-            <div class="card text-center">
+            <div class="card text-center mt-2">
                 <div class="card-header">Sessions Per Month</div>
                 <div class="card-body">
                     <h2>{{ $stats['sessionsPerMonth'] }}</h2>
                 </div>
             </div>
-            <div class="card text-center">
+            <div class="card text-center mt-2">
                 <div class="card-header">Unique Students</div>
                 <div class="card-body">
                     <h2>{{ $stats['uniqueStudents'] }}</h2>
                 </div>
             </div>
-            <div class="card text-center">
+            <div class="card text-center mt-2">
                 <div class="card-header">OTS Pass/Fail</div>
                 <div class="card-body">
                     <h2><span class="text-success">{{ $stats['otsPerMonth']['pass'] }}</span> / <span class="text-danger">{{ $stats['otsPerMonth']['fail'] }}</span></h2>
                 </div>
             </div>
-            <div class="card text-center">
+            <div class="card text-center mt-2">
                 <div class="card-header">Student<br />Cancellations</div>
                 <div class="card-body">
                     <h2>{{ $stats['studentCancel'] }}</h2>
                 </div>
             </div>
-            <div class="card text-center">
+            <div class="card text-center mt-2">
                 <div class="card-header">Student<br />No-Shows</div>
                 <div class="card-body">
                     <h2>{{ $stats['studentNoShow'] }}</h2>
@@ -61,12 +65,12 @@ Training Statistics
             </div>
             <br />
             <div class="card">
-                {{ Html::image('/dashboard/training/statistics/graph?id=2&year=' . $stats['dateSelect']['year'] . '&month=' . $stats['dateSelect']['month'], 'Average session duration') }}
+                {{ Html::image('/dashboard/training/statistics/graph?id=2&year=' . $stats['dateSelect']['year'] . '&month=' . $stats['dateSelect']['month'], 'Sessions by instructor and type') }}
             </div>
         </div>
         <div class="col-sm-5 col-xs-12">
             <div class="card">
-                {{ Html::image('/dashboard/training/statistics/graph?id=3&year=' . $stats['dateSelect']['year'] . '&month=' . $stats['dateSelect']['month'], 'Sessions by instructor and type') }}
+                {{ Html::image('/dashboard/training/statistics/graph?id=3&year=' . $stats['dateSelect']['year'] . '&month=' . $stats['dateSelect']['month'], 'Average session duration') }}
             </div>
             <br />
             <div class="card">
@@ -76,12 +80,12 @@ Training Statistics
     </div>
     <div class="row">
         <div class="col-12">
-            <div class="card">
+            <div class="card mt-2">
                 <div class="card-header">TA's Monthly Report
-                    <div class="float-right"><i class="fas fa-clipboard" onclick="copyToClipboard('taReport');"></i></div>
+                    <div class="float-right"><i class="fas fa-clipboard" onclick="copyToClipboard('taReport');" title="Copy to clipboard"></i></div>
                 </div>
                 <div class="card-body">
-                    <textarea id="taReport" rows="3" class="form-control">{{ $stats['taMonthlyReport'] }}</textarea>
+                    <textarea id="taReport" rows="3" class="form-control" readonly>{{ $stats['taMonthlyReport'] }}</textarea>
                 </div>
             </div>
         </div>
@@ -90,5 +94,5 @@ Training Statistics
 @else
 <h3>Insufficient permissions to view this page</h3>
 @endif
-
+{{Html::script(asset('js/helpers.js'))}}
 @endsection
