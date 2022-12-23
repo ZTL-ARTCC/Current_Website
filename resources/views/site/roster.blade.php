d6caa78ff00bb9cebf6bb57db1868e5c0addef14
kjporter <kporter@kplink.net>
2022-12-23 12:44:07 -0700

    Roster cleanup
    Adds AWM tag, removes visit agreement commented sections of code

--- resources/views/site/roster.blade.php
+++ resources/views/site/roster.blade.php
@@ -48,9 +48,6 @@ Roster
         <li class="nav-item">
             <a class="nav-link" href="#visit" role="tab" data-toggle="tab" style="color:black">Visiting Controllers</a>
         </li>
-        <!--<li class="nav-item">
-            <a class="nav-link" href="#visitagree" role="tab" data-toggle="tab" style="color:black">ZJX Visiting Controllers</a>
-        </li> -->
     </ul>
     <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="home">
@@ -80,6 +77,8 @@ Roster
                                     <span class="badge badge-danger">TA</span> {{ $c->backwards_name }}
                                 @elseif($c->hasRole('wm'))
                                     <span class="badge badge-primary">WM</span> {{ $c->backwards_name }}
+                                @elseif($c->hasRole('awm'))
+                                    <span class="badge badge-primary">AWM</span> {{ $c->backwards_name }}
                                 @elseif($c->hasRole('ec'))
                                     <span class="badge badge-primary">EC</span> {{ $c->backwards_name }}
                                 @elseif($c->hasRole('aec'))
@@ -219,78 +218,6 @@ Roster
                 </tbody>
             </table>
         </div>
-		<?php
-		/*
-        <!--<div role="tabpanel" class="tab-pane" id="visitagree">
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col"><center>Initials</center></th>
-                        <th scope="col"><center>Rating</center></th>
-                        <th scope="col"><center>Status</center></th>
-                        <th scope="col"><center>Delivery</center></th>
-                        <th scope="col"><center>Ground</center></th>
-                        <th scope="col"><center>Tower</center></th>
-                        <th scope="col"><center>Approach</center></th>
-                        <th scope="col"><center>Center</center></th>
-                    </tr>
-                </thead>
-                <tbody>
-				@foreach($visagreecontrollers as $c)
-					@if($c->rating_short != "OBS" && $c->status != 2)
-                            <tr>
-                                <td>{{ $c->backwards_name }} - {{ $c->visitor_from }}</td>
-                                <td><center>{{$c->initials}}</center></td>
-                                <td><center>{{ $c->rating_short }}</center></td>
-                                <td><center>{{ $c->status_text }}</center></td>
-                                @if($c->del == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->del == 1)
-                                    <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                                @elseif($c->del == 2)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @endif
-                                @if($c->gnd == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->gnd == 1)
-                                    <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                                @elseif($c->gnd == 2)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @endif
-                                @if($c->twr == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->twr == 1)
-                                    <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                                @elseif($c->twr == 2)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @endif
-                                @if($c->app == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->app == 1)
-                                    <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                                @elseif($c->app == 2)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @elseif($c->app == 90)
-                                    <td><center><b style="color:blue;">A80 SAT</b></center></td>
-                                @elseif($c->app == 91)
-                                    <td><center><b style="color:blue;">A80 DR</b></center></td>
-                                @elseif($c->app == 92)
-                                    <td><center><b style="color:blue;">A80 TAR</b></center></td> 
-                                @endif
-                                @if($c->ctr == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->ctr == 1)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @endif
-                            </tr>
-                        @endif
-                    @endforeach
-                </tbody>
-            </table>
-        </div> -->
-		*/
-		?>
     </div>
 </div>
 
