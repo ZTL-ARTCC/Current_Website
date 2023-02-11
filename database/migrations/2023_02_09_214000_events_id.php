3234849cd7ee78ff86946d1e506cf550038e6c6d
kjporter <kporter@kplink.net>
2023-02-11 11:47:57 -0700

    Adds database migration for id_topic column on events table

---
+++ database/migrations/2023_02_09_214000_events_id.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class Events extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('events', function ($table) {
+            $table->integer('id_topic')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('events', function ($table) {
+            $table->dropColumn('id_topic');
+        });
+    }
+}
