bbc9c15079eed505ac5d1215a9714151c68f8a00
kjporter <kporter@kplink.net>
2023-05-28 15:54:59 -0700

    Fixes timestamp name

--- app/SetmoreAppointment.php
+++ app/SetmoreAppointment.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class SetmoreAppointment extends Model {
     protected $table = 'setmore';
-    protected $fillable = ['id', 'setmore_key', 'start_time', 'duration', 'staff_key', 'staff_name', 'service_key', 'service_description', 'customer_cid', 'updated_at'];
+    public $timestamps = false;
+    protected $fillable = ['id', 'setmore_key', 'start_time', 'duration', 'staff_key', 'staff_name', 'service_key', 'service_description', 'customer_cid', 'created_at'];
 }
