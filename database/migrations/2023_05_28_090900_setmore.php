9fe2751c8fad4529b07990936b8d8be1bd01c53b
kjporter <kporter@kplink.net>
2023-05-28 11:34:20 -0700

    Update formatting

--- database/migrations/2023_05_28_090900_setmore.php
+++ database/migrations/2023_05_28_090900_setmore.php
@@ -13,13 +13,13 @@ class Roster extends Migration {
     public function up() {
         Schema::create('setmore', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('setmore_key',10);
+            $table->string('setmore_key', 10);
             $table->datetime('start_time');
             $table->integer('duration')->nullable();
-            $table->string('staff_key',9);
-            $table->string('staff_name',100)->nullable();
-            $table->string('service_key',9);
-            $table->string('service_description',100)->nullable();
+            $table->string('staff_key', 9);
+            $table->string('staff_name', 100)->nullable();
+            $table->string('service_key', 9);
+            $table->string('service_description', 100)->nullable();
             $table->integer('customer_cid')->nullable();
             $table->timestamp('updated_at')->useCurrent();
         });
