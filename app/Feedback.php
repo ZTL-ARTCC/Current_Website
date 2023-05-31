5b95887998bb96c27c77f67cff7dec26df0e5494
kjporter <kporter@kplink.net>
2023-05-31 14:18:23 -0700

    Updated attribute name and added alias

--- app/Feedback.php
+++ app/Feedback.php
@@ -27,14 +27,18 @@ class Feedback extends Model {
         }
     }
 
-    public function getControllerNameAttribute() {
-        $controller = User::find($this->controller_id);
-        $event = Event::find($this->controller_id);
+    public function getFeedbackIdAttribute() {
+        return $this->controller_id;
+    }
+
+    public function getFeedbackNameAttribute() {
+        $controller = User::find($this->feedback_id);
+        $event = Event::find($this->feedback_id);
         if (isset($controller)) {
             $name = $controller->full_name;
         } elseif (isset($event)) {
             $name = $event->name;
-        } elseif ($this->controller_id == 0) {
+        } elseif ($this->feedback_id == 0) {
             $name = 'General ATC Feedback';
         } else {
             $name = '[This controller is no longer a member]';
