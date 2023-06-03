95ca7dfd16098a182b4dc60bdfaaba3f33207f04
kjporter <kporter@kplink.net>
2023-06-03 16:51:42 -0700

    Update models

--- app/SetmoreAppointment.php
+++ app/SetmoreAppointment.php
@@ -7,5 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class SetmoreAppointment extends Model {
     protected $table = 'setmore';
     public $timestamps = false;
-    protected $fillable = ['id', 'setmore_key', 'start_time', 'duration', 'staff_key', 'staff_name', 'service_key', 'service_description', 'customer_cid', 'created_at'];
 }
