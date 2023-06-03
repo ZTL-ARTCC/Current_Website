31399f1fc672dad1e0302993dab8229faaa1dad7
kjporter <kporter@kplink.net>
2023-06-03 16:51:55 -0700

    Add load table to migration

--- database/migrations/2023_05_28_090900_setmore_appointments.php
+++ database/migrations/2023_05_28_090900_setmore_appointments.php
@@ -23,6 +23,18 @@ return new class extends Migration {
             $table->integer('customer_cid')->nullable();
             $table->timestamp('created_at')->useCurrent();
         });
+        Schema::create('setmore_load', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('setmore_key', 50);
+            $table->datetime('start_time');
+            $table->integer('duration')->nullable();
+            $table->string('staff_key', 18);
+            $table->string('staff_name', 100)->nullable();
+            $table->string('service_key', 41);
+            $table->string('service_description', 100)->nullable();
+            $table->integer('customer_cid')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+        });
     }
 
     /**
@@ -32,5 +44,6 @@ return new class extends Migration {
      */
     public function down() {
         Schema::dropIfExists('setmore');
+        Schema::dropIfExists('setmore_load');
     }
 };
