51477d71122e2aa07348cfd06873deb1f7c0c45b
kjporter <kporter@kplink.net>
2023-08-16 07:38:05 -0400

    Fix format

--- app/LocalHero.php
+++ app/LocalHero.php
@@ -2,8 +2,6 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
 class LocalHero extends Bronze {
     protected $table = 'local_hero';
     protected $fillable = ['id', 'controller_id', 'month', 'year', 'month_hours', 'updated_at', 'created_at'];
