2ada91ddaa316e8f5eed7f5b49cf879cac30d125
kjporter <kporter@kplink.net>
2023-08-16 20:06:03 -0400

    Fix button formatting

--- resources/views/dashboard/admin/bronze-mic.blade.php
+++ resources/views/dashboard/admin/bronze-mic.blade.php
@@ -59,28 +59,30 @@ if ($month == 12) { $nm = 1; $nyr = $year + 1; } else { $nm = $month + 1; $nyr =
                     <td>{{ $stats[$h->id]->bronze_hrs }}</td>
                     <td>{{ $year_stats[$h->id]->bronze_hrs }}</td>
                     <td>
-                        @toggle('local-hero')
-                            @if($winner_local == null)
-                                {!! Form::open(['url' => '/dashboard/admin/local-hero/'.$year.'/'.$month.'/'.$stats[$h->id]->local_hero_hrs.'/'.$h->id]) !!}
+                        <div class="btn-group">
+                            @toggle('local-hero')
+                                @if($winner_local == null)
+                                    {!! Form::open(['url' => '/dashboard/admin/local-hero/'.$year.'/'.$month.'/'.$stats[$h->id]->local_hero_hrs.'/'.$h->id]) !!}
+                                        @csrf
+                                        <button action="submit" class="btn btn-primary btn-simple-tooltip" data-toggle="tooltip" title="Set as local hero Winner for <?=$mname?>"><i class="fas fa-trophy"></i></button>
+                                    {!! Form::close() !!}
+                                @elseif($winner_local->controller_id == $h->id)
+                                    <a href="/dashboard/admin/local-hero/remove/{{ $winner_local->id }}/{{ $year }}/{{ $month }}" class="btn btn-danger btn-simple-tooltip" data-toggle="tooltip" title="Remove local hero Winner"><i class="fas fa-times"></i></a>
+                                @else
+                                    <p>Winner already selected.</p>
+                                @endif
+                            @endtoggle
+                            @if($winner == null)
+                                {!! Form::open(['url' => '/dashboard/admin/bronze-mic/'.$year.'/'.$month.'/'.$stats[$h->id]->bronze_hrs.'/'.$h->id]) !!}
                                     @csrf
-                                    <button action="submit" class="btn btn-primary btn-simple-tooltip" data-toggle="tooltip" title="Set as local hero Winner for <?=$mname?>"><i class="fas fa-trophy"></i></button>
+                                    <button action="submit" class="btn btn-simple-tooltip" style="background-color:#C9AE5D" data-toggle="tooltip" title="Set as bronze Winner for <?=$mname?>"><i class="fa fa-microphone"></i></button>
                                 {!! Form::close() !!}
-                            @elseif($winner_local->controller_id == $h->id)
-                                <a href="/dashboard/admin/local-hero/remove/{{ $winner_local->id }}/{{ $year }}/{{ $month }}" class="btn btn-danger btn-simple-tooltip" data-toggle="tooltip" title="Remove local hero Winner"><i class="fas fa-times"></i></a>
+                            @elseif($winner->controller_id == $h->id)
+                                <a href="/dashboard/admin/bronze-mic/remove/{{ $winner->id }}/{{ $year }}/{{ $month }}" class="btn btn-danger btn-simple-tooltip" data-toggle="tooltip" title="Remove bronze mic Winner"><i class="fas fa-times"></i></a>
                             @else
                                 <p>Winner already selected.</p>
                             @endif
-                        @endtoggle
-                        @if($winner == null)
-                            {!! Form::open(['url' => '/dashboard/admin/bronze-mic/'.$year.'/'.$month.'/'.$stats[$h->id]->bronze_hrs.'/'.$h->id]) !!}
-                                @csrf
-                                <button action="submit" class="btn btn-simple-tooltip" style="background-color:#C9AE5D" data-toggle="tooltip" title="Set as bronze Winner for <?=$mname?>"><i class="fa fa-microphone"></i></button>
-                            {!! Form::close() !!}
-                        @elseif($winner->controller_id == $h->id)
-                            <a href="/dashboard/admin/bronze-mic/remove/{{ $winner->id }}/{{ $year }}/{{ $month }}" class="btn btn-danger btn-simple-tooltip" data-toggle="tooltip" title="Remove bronze mic Winner"><i class="fas fa-times"></i></a>
-                        @else
-                            <p>Winner already selected.</p>
-                        @endif
+                        </div>
                     </td>
                 </tr>
             @endforeach
