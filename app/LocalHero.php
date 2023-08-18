f65614f432dccdca7dda9ce3a53dc45d36db2123
kjporter <kporter@kplink.net>
2023-08-18 17:50:08 -0400

    Removes unnecessary fillable property from modal

--- app/LocalHero.php
+++ app/LocalHero.php
@@ -4,5 +4,4 @@ namespace App;
 
 class LocalHero extends Bronze {
     protected $table = 'local_hero';
-    protected $fillable = ['id', 'controller_id', 'month', 'year', 'month_hours', 'updated_at', 'created_at'];
 }
