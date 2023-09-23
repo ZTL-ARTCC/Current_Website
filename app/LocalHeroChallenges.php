96e4e004500c2e05e21c3fc7e5707799d4845c7f
kjporter <kporter@kplink.net>
2023-09-23 07:48:27 -0400

    Fixes facility list, adds class Cs as locals

--- app/LocalHeroChallenges.php
+++ app/LocalHeroChallenges.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LocalHeroChallenges extends Model {
     protected $table = 'local_hero_challenges';
     private static $enroute = ['ZTL'];
-    private static $approach = ['ATL','CLT','BHM','GSP','AVL','GSO','TYS','CHA','FTY','RYY','AHN'];
-    private static $local = ['AGS','GMU','GYH','TCL','MXF','MGM','LSF','CSG','MCN','WRB','JQF','VUJ','INT','TRI','LZU','ASN','HKY','PDK'];
+    private static $approach = ['ATL','CLT','BHM','GSP','AVL','GSO','TYS','CHA','AHN'];
+    private static $local = ['AGS','FTY','RYY','GMU','GYH','TCL','MXF','MGM','LSF','CSG','MCN','WRB','JQF','VUJ','INT','TRI','LZU','ASN','HKY','PDK'];
 
     public static function getLocalHeroChallengePositions() {
         $control_positions = [];
@@ -18,7 +18,8 @@ class LocalHeroChallenges extends Model {
         foreach (static::$approach as $facility) {
             $control_positions[] = $facility . '_APP';
         }
-        foreach (static::$local as $facility) {
+        $locals = array_merge(static::$approach, static::$local);
+        foreach ($locals as $facility) {
             $control_positions[] = $facility . '_TWR';
             $control_positions[] = $facility . '_GND';
             $control_positions[] = $facility . '_DEL';
