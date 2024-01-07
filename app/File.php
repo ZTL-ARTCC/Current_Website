ed1b01a67c7b34ccd16c0551cfd22d119d2d653e
kjporter <kporter@kplink.net>
2024-01-07 14:26:53 -0500

    Updates file types

--- app/File.php
+++ app/File.php
@@ -9,13 +9,11 @@ class File extends Model {
     protected $fillable = ['id', 'name', 'type', 'desc', 'path', 'disp_order', 'permalink', 'created_at', 'updated_at', 'row_separator'];
 
     public static $WordType = [
-        0 => 'VRC',
-        1 => 'vSTARS',
-        2 => 'vERAM',
         3 => 'vATIS',
         4 => 'SOPs',
         5 => 'LOAs',
-        6 => 'Staff'
+        6 => 'Staff',
+        7 => 'Training'
     ];
 
     public function getWordTypeAttribute() {
