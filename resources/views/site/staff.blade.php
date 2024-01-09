c704fc764c49255eeee8bc74010c191fee8785c8
Evanston09 <evanhkim09@gmail.com>
2024-01-09 15:55:02 -0500

    Updates ATM description for non-controllers too

--- resources/views/site/staff.blade.php
+++ resources/views/site/staff.blade.php
@@ -27,7 +27,7 @@ Staff
             @endif
             &nbsp;<a href="mailto:atm@ztlartcc.org" style="color:black"><i class="fa fa-envelope" ></i></a>
         </h4>
-        <p>The Air Traffic Manager is responsible to the VATUSA Southern Region Director for the overall administration of the ARTCC. The ATM is responsible for appointing ARTCC staff members and delegation of authorities.</p>
+        <p>The Air Traffic Manager is responsible to the Deputy Director - Air Traffic Services for the overall administration of the ARTCC. The ATM is responsible for appointing ARTCC staff members and delegation of authorities.</p>
     </div>
     <hr>
     <div class="block-heading-two">
