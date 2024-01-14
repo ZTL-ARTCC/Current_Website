7c358168b0c137fddedd2663f149f2a201b19ecf
kjporter <kporter@kplink.net>
2024-01-13 20:35:37 -0500

    Updates roster views to the new format

--- resources/views/site/roster.blade.php
+++ resources/views/site/roster.blade.php
@@ -4,6 +4,10 @@
 Roster
 @endsection
 
+@push('custom_header')
+<link rel="stylesheet" href="{{ asset('css/roster.css') }}" />
+@endpush
+
 @section('content')
 <span class="border border-light" style="background-color:#F0F0F0">
     <div class="container">
@@ -15,210 +19,136 @@ Roster
 <br>
 
 <div class="container">
-    <h5>Certification Key:</h5>
-    <div class="row">
-        <div class="col-sm-2">
-            <p>No Certification:</p>
-        </div>
-        <div class="col-sm-2">
-            <i class="fas fa-times" style="color:red"></i>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-2">
-            <p>Minor Certification:</p>
-        </div>
-        <div class="col-sm-2">
-            <i class="far fa-check-circle" style="color:green"></i>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-2">
-            <p>Full (Minor/Major) Certification:</p>
-        </div>
-        <div class="col-sm-2">
-            <i class="fas fa-check" style="color:green"></i>
-        </div>
-    </div>
-    <br>
     <ul class="nav nav-tabs nav-justified" role="tablist">
         <li class="nav-item">
-            <a class="nav-link active" href="#home" role="tab" data-toggle="tab" style="color:black">Home Controllers</a>
+            <a class="nav-link active" href="#hcontrollers" role="tab" data-toggle="tab" style="color:black"><i class="fas fa-home"></i>&nbsp;Home Controllers</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#visit" role="tab" data-toggle="tab" style="color:black">Visiting Controllers</a>
+            <a class="nav-link" href="#vcontrollers" role="tab" data-toggle="tab" style="color:black"><i class="fas fa-suitcase"></i>&nbsp;Visiting Controllers</a>
         </li>
     </ul>
+    @php
+    $tabs = ['hcontrollers', 'vcontrollers'];
+    @endphp
     <div class="tab-content">
-        <div role="tabpanel" class="tab-pane active" id="home">
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col"><center>Initials</center></th>
-                        <th scope="col"><center>Rating</center></th>
-                        <th scope="col"><center>Status</center></th>
-                        <th scope="col"><center>Delivery</center></th>
-                        <th scope="col"><center>Ground</center></th>
-                        <th scope="col"><center>Tower</center></th>
-                        <th scope="col"><center>Approach</center></th>
-                        <th scope="col"><center>Center</center></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($hcontrollers as $c)
+        @foreach($tabs as $tab)
+        @if($loop->first)
+        <div role="tabpanel" class="tab-pane active" id="{{ $tab }}">
+            @else
+            <div role="tabpanel" class="tab-pane" id="{{ $tab }}">
+                @endif
+                <table class="table table-bordered table-striped">
+                    <thead class="sticky">
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col" class="text-center">Initials</th>
+                            <th scope="col" class="text-center">Rating</th>
+                            <th scope="col" class="text-center">Status</th>
+                            <th scope="col" class="text-center">Unrestricted<br>Fields</th>
+                            <th scope="col" class="text-center">CLT<br>Tier 1</th>
+                            <th scope="col" class="text-center">ATL<br> Tier 1</th>
+                            <th scope="col" class="text-center">ZTL<br>Enroute</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($$tab as $c)
                         <tr>
                             <td>
                                 @if($c->hasRole('atm'))
-                                    <span class="badge badge-danger">ATM</span> {{ $c->backwards_name }}
+                                <span class="badge badge-danger">ATM</span>
                                 @elseif($c->hasRole('datm'))
-                                    <span class="badge badge-danger">DATM</span> {{ $c->backwards_name }}
+                                <span class="badge badge-danger">DATM</span>
                                 @elseif($c->hasRole('ta'))
-                                    <span class="badge badge-danger">TA</span> {{ $c->backwards_name }}
+                                <span class="badge badge-danger">TA</span>
                                 @elseif($c->hasRole('wm'))
-                                    <span class="badge badge-primary">WM</span> {{ $c->backwards_name }}
+                                <span class="badge badge-primary">WM</span>
                                 @elseif($c->hasRole('awm'))
-                                    <span class="badge badge-primary">AWM</span> {{ $c->backwards_name }}
+                                <span class="badge badge-primary">AWM</span>
                                 @elseif($c->hasRole('ec'))
-                                    <span class="badge badge-primary">EC</span> {{ $c->backwards_name }}
+                                <span class="badge badge-primary">EC</span>
                                 @elseif($c->hasRole('aec'))
-                                    <span class="badge badge-primary">AEC</span> {{ $c->backwards_name }}
+                                <span class="badge badge-primary">AEC</span>
                                 @elseif($c->hasRole('fe'))
-                                    <span class="badge badge-primary">FE</span> {{ $c->backwards_name }}
+                                <span class="badge badge-primary">FE</span>
                                 @elseif($c->hasRole('afe'))
-                                    <span class="badge badge-primary">AFE</span> {{ $c->backwards_name }}
+                                <span class="badge badge-primary">AFE</span>
                                 @elseif($c->hasRole('ins'))
-                                    <span class="badge badge-info">INS</span> {{ $c->backwards_name }}
+                                <span class="badge badge-info">INS</span>
                                 @elseif($c->hasRole('mtr'))
-                                    <span class="badge badge-info">MTR</span> {{ $c->backwards_name }}
-                                @else
-                                    {{ $c->backwards_name }}
+                                <span class="badge badge-info">MTR</span>
+                                @endif
+                                {{ $c->backwards_name }}
+                            </td>
+                            <td class="text-center">{{$c->initials}}</td>
+                            <td class="text-center">{{ $c->rating_short }}</td>
+                            <td class="text-center">{{ $c->status_text }}</td>
+                            <!-- Unrestricted -->
+                            <td class="text-center">
+                                @if($c->gnd > 0)
+                                <span class="badge badge-primary">DEL</span>
+                                <span class="badge badge-success">GND</span>
+                                @endif
+                                @if($c->twr == 99)
+                                <span class="badge badge-warning text-light" data-toggle="tooltip" data-html="true" title="Cert Expires: {{ $c->solo }}<br>{{$c->twr_solo_fields}}<br>Auth Expires: {{$c->twr_solo_expires}}">GND-SOLO</span>
+                                @elseif($c->twr > 0)
+                                <span class="badge badge-danger">TWR</span>
+                                @endif
+                                @if($c->app == 99)
+                                <span class="badge badge-warning text-light" data-toggle="tooltip" style="color:#c1ad13" title="Cert Expires: {{ $c->solo }}<br>{{$c->twr_solo_fields}}<br>Auth Expires: {{$c->twr_solo_expires}}">APP-SOLO</span>
+                                @elseif($c->app > 0)
+                                <span class="badge badge-info">APP</span>
                                 @endif
                             </td>
-                            <td><center>{{$c->initials}}</center></td>
-                            <td><center>{{ $c->rating_short }}</center></td>
-                            <td><center>{{ $c->status_text }}</center></td>
-                            @if($c->del == 0)
-                                <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                            @elseif($c->del == 1)
-                                <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                            @elseif($c->del == 2)
-                                <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                            @endif
-                            @if($c->gnd == 0)
-                                <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                            @elseif($c->gnd == 1)
-                                <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                            @elseif($c->gnd == 2)
-                                <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                            @endif
-                            @if($c->twr == 0)
-                                <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                            @elseif($c->twr == 1)
-                                <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                            @elseif($c->twr == 2)
-                                <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                            @elseif($c->twr == 99)
-                                <td><center><i class="fab fa-stripe-s" data-toggle="tooltip" style="color:#c1ad13" title="Expires: {{ $c->solo }}"></i></center></td>
-                            @endif
-                            @if($c->app == 0)
-                                <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                            @elseif($c->app == 1)
-                                <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                            @elseif($c->app == 2)
-                                <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                            @elseif($c->app == 90)
-                                <td><center><b style="color:blue;">A80 SAT</b></center></td>
-                            @elseif($c->app == 91)
-                                <td><center><b style="color:blue;">A80 DR</b></center></td>
-                            @elseif($c->app == 92)
-                                <td><center><b style="color:blue;">A80 TAR</b></center></td>    
-                            @elseif($c->app == 99)
-                                <td><center><i class="fab fa-stripe-s" data-toggle="tooltip" style="color:#c1ad13" title="Expires: {{ $c->solo }}"></i></center></td>
-                            @elseif($c->app == 89)
-                                <td><center style="color:#c1ad13">M</center></td>
-                            @endif
-                            @if($c->ctr == 0)
-                                <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                            @elseif($c->ctr == 1)
-                                <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                            @elseif($c->ctr == 99)
-                                <td><center><i class="fab fa-stripe-s" data-toggle="tooltip" style="color:#c1ad13" title="Expires: {{ $c->solo }}"></i></center></td>
-                            @endif
+                            <!-- CLT Tier 1 -->
+                            <td class="text-center">
+                                @if(($c->clt_gnd > 0)||($c->gnd == 2))
+                                <span class="badge badge-primary">DEL</span>
+                                <span class="badge badge-success">GND</span>
+                                @endif
+                                @if(($c->clt_twr > 0)||($c->twr == 2))
+                                <span class="badge badge-danger">TWR</span>
+                                @endif
+                                @if(($c->clt_app > 0)||($c->app > 1))
+                                <span class="badge badge-info">APP</span>
+                                @endif
+                            </td>
+                            <!-- ATL Tier 1 -->
+                            <td class="text-center">
+                                @if(($c->atl_gnd > 0)||($c->gnd == 2))
+                                <span class="badge badge-primary">DEL</span>
+                                <span class="badge badge-success">GND</span>
+                                @endif
+                                @if(($c->atl_twr > 0)||($c->twr == 2))
+                                <span class="badge badge-danger">TWR</span>
+                                @endif
+                                @if(($c->atl_app > 0)||($c->app > 1))
+                                <span class="badge badge-info">
+                                    @if($c->app == 90)
+                                    SAT
+                                    @elseif($c->app == 91)
+                                    DR
+                                    @elseif($c->app == 92)
+                                    TAR
+                                    @else
+                                    APP
+                                    @endif
+                                </span>
+                                @endif
+                            </td>
+                            <!-- Enroute -->
+                            <td class="text-center">
+                                @if($c->ctr == 99)
+                                <span class="badge badge-warning text-light" data-toggle="tooltip" title="Cert Expires: {{ $c->solo }}<br>{{$c->twr_solo_fields}}<br>Auth Expires: {{$c->twr_solo_expires}}">ZTL-SOLO</span>
+                                @elseif($c->ctr > 0)
+                                <span class="badge badge-secondary">ZTL</span>
+                                @endif
+                            </td>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div role="tabpanel" class="tab-pane" id="visit">
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col"><center>Initials</center></th>
-                        <th scope="col"><center>Rating</center></th>
-                        <th scope="col"><center>Status</center></th>
-                        <th scope="col"><center>Delivery</center></th>
-                        <th scope="col"><center>Ground</center></th>
-                        <th scope="col"><center>Tower</center></th>
-                        <th scope="col"><center>Approach</center></th>
-                        <th scope="col"><center>Center</center></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($vcontrollers as $c)
-                        @if($c->status != 2)
-                            <tr>
-                                <td>{{ $c->backwards_name }} - {{ $c->visitor_from }}</td>
-                                <td><center>{{$c->initials}}</center></td>
-                                <td><center>{{ $c->rating_short }}</center></td>
-                                <td><center>{{ $c->status_text }}</center></td>
-                                @if($c->del == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->del == 1)
-                                    <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                                @elseif($c->del == 2)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @endif
-                                @if($c->gnd == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->gnd == 1)
-                                    <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                                @elseif($c->gnd == 2)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @endif
-                                @if($c->twr == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->twr == 1)
-                                    <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                                @elseif($c->twr == 2)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @endif
-                                @if($c->app == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->app == 1)
-                                    <td><center><i class="far fa-check-circle" style="color:green"></i></center></td>
-                                @elseif($c->app == 2)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @elseif($c->app == 90)
-                                    <td><center><b style="color:blue;">A80 SAT</b></center></td>
-                                @elseif($c->app == 91)
-                                    <td><center><b style="color:blue;">A80 DR</b></center></td>
-                                @elseif($c->app == 92)
-                                    <td><center><b style="color:blue;">A80 TAR</b></center></td>     
-                                @endif
-                                @if($c->ctr == 0)
-                                    <td><center><i class="fas fa-times" style="color:red"></i></center></td>
-                                @elseif($c->ctr == 1)
-                                    <td><center><i class="fas fa-check" style="color:green"></i></center></td>
-                                @endif
-                            </tr>
-                        @endif
-                    @endforeach
-                </tbody>
-            </table>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            @endforeach
         </div>
     </div>
-</div>
-
-@endsection
+    @endsection
