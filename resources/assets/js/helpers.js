63f7eab143b388bb09555e669360f1b317398bc3
kjporter <kporter@kplink.net>
2024-01-15 10:37:03 -0500

    Moves assets to resources

--- resources/assets/js/helpers.js
+++ resources/assets/js/helpers.js
@@ -1,4 +1,4 @@
-export function copyToClipboard(eId) {
-  let e = document.getElementById(eId);
-  navigator.clipboard.writeText(e.innerHTML);
+function copyToClipboard(eId) {
+  let e = document.getElementById(eId)
+  navigator.clipboard.writeText(e.innerHTML)
 }
