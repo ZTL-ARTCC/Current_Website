05e69433589896c60a802a16bc924cb7e32a7c3c
kjporter <kporter@kplink.net>
2024-02-07 20:55:30 -0500

    Updates stylesheets

--- resources/views/dashboard/controllers/profile.blade.php
+++ resources/views/dashboard/controllers/profile.blade.php
@@ -252,7 +252,7 @@ Profile
             <br>
             <div class="container">
                 <p>Opting into emails will only affect the recieving of mass emails. If you elect to opt into emails, you agree to recieve mass emails sent to groups of members of the vZTL ARTCC. This selection will not affect the reception of personalized emails (both automated and issued by staff) for example, training ticket emails. If you have any questions, please contact the ATM at <a href="mailto:atm@ztlartcc.org">atm@ztlartcc.org</a>.</p>
-                <p>You may opt out at any time by using the slider shown at the top of the controller dashboard at all times.</p>
+                <p>You may opt out at any time by using the slider shown on the profile page.</p>
                 <br>
                 <i>Please check the following check boxes if you would like to continue.</i>
                 <hr>
