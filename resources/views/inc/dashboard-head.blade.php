1e17585a052bb508df697198e7504e31ec8b6074
kjporter <kporter@kplink.net>
2024-02-18 20:46:50 -0500

    Define image widths in img tag

--- resources/views/inc/dashboard-head.blade.php
+++ resources/views/inc/dashboard-head.blade.php
@@ -8,24 +8,24 @@
                 @endphp
 
                 @if(($month == 12 && $day == 31) || ($month == 1 && $day == 1))
-                    <img src="/photos/logos/ztl_logo_fireworks-black.png">
+                    <img width="100" src="/photos/logos/ztl_logo_fireworks-black.png">
                 @elseif(($month >= 3 && $day >= 20) && $month <= 4)
-                    <img src="/photos/logos/ztl_logo_spring-black.png">
+                    <img width="100" src="/photos/logos/ztl_logo_spring-black.png">
                 @elseif(($month == 1 && $day == 16) || ($month == 2 && $day == 20) || ($month == 5 && $day == 29) || ($month == 6 && $day == 19) || ($month == 7 && $day == 4) || ($month == 9 && $day == 4) || ($month == 10 && $day == 10))
-                    <img src="/photos/logos/ztl_logo_flag-black.png">
+                    <img width="100" src="/photos/logos/ztl_logo_flag-black.png">
                 @elseif($month >= 6 && $month <= 9)
-                    <img src="/photos/logos/ztl__logo_beach-black.png">
+                    <img width="100" src="/photos/logos/ztl__logo_beach-black.png">
                 @elseif($month == 10)
-                    <img src="/photos/logos/ztl_logo_halloween-black.png">
+                    <img width="100" src="/photos/logos/ztl_logo_halloween-black.png">
                 @elseif($month == 11 && $day >= 20)
-                    <img src="/photos/logos/ztl_logo_turkey-black.png">
+                    <img width="100" src="/photos/logos/ztl_logo_turkey-black.png">
                 @elseif($month == 12)
-                    <img src="/photos/logos/ztl_logo_santa-black.png">
+                    <img width="100" src="/photos/logos/ztl_logo_santa-black.png">
                 @else
-                    <img src="/photos/logos/0_ztl_logo_black.png">
+                    <img width="100" src="/photos/logos/0_ztl_logo_black.png">
                 @endif
             @else
-                <img src="/photos/logos/0_ztl_logo_black.png">
+                <img width="100" src="/photos/logos/0_ztl_logo_black.png">
             @endtoggle
         </a>
             <ul class="navbar-nav">
