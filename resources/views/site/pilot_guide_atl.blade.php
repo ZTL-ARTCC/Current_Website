fae3009ec9bfd1b37e81653f695a5d821f8f40b9
JustARaceThing <jdbass107@gmail.com>
2024-02-24 13:27:56 -0500

    fixes typo

--- resources/views/site/pilot_guide_atl.blade.php
+++ resources/views/site/pilot_guide_atl.blade.php
@@ -249,7 +249,7 @@ return $badge;
             </div>
             <h3>Receiving a clearance</h3>
             <p> After filing a flight plan, you should receive your clearance from ATC. If you have not received a clearance after a reasonable
-                about of time, you should call {!! fetchBadge($del)['position'] !!} on {!! fetchBadge($del)['frequency'] !!} and request your
+                amount of time, you should call {!! fetchBadge($del)['position'] !!} on {!! fetchBadge($del)['frequency'] !!} and request your
                 clearance.</p>
             <p class="font-italic">Example: Atlanta Clearance, Delta 1234 clearance to Charlotte ready to copy.</p>
             <p>Aircraft with CPDLC capabilities should expect to receive a computerized pre-departure clearance from ATC. Because ATC sends this
