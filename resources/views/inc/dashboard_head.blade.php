dbfc4aeb1bfd01591d622f66dc79097debeca9b4
kjporter <kporter@kplink.net>
2024-03-22 11:59:47 -0400

    Insert logo component

---
+++ resources/views/inc/dashboard_head.blade.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="/dashboard">
+            @include('inc.logo', ['color' => 'black'])
+        </a>
+            <ul class="navbar-nav">
+                {!! Form::open(['action' => 'ControllerDash@searchAirport']) !!}
+                    <div class="form-inline">
+                        {!! Form::text('apt', null, ['placeholder' => 'Search Airport ICAO', 'class' => 'form-control']) !!}
+                        &nbsp;
+                        <button class="btn btn-success" type="submit">Search</button>
+                    </div>
+                {!! Form::close() !!}
+            </ul>
+            &nbsp;&nbsp;&nbsp;
+
+            <ul class="navbar-nav ml-auto">
+			<a class="nav-link {{ Nav::isRoute('controller_dash_home') }}" href="/dashboard">Dashboard Home</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" style="pointer-events:none">{{ Auth::user()->full_name }} - {{ Auth::user()->rating_short }}</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
