c9d613978b0615b1f78f98baac20fa6b27da3c32
kjporter <kporter@kplink.net>
2024-04-21 07:51:36 -0400

    Offset scheduled tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,20 +28,16 @@ class Kernel extends ConsoleKernel {
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void {
-        $schedule->command('cache:prune-stale-tags')->hourly();
-        $schedule->command('SoloCerts:UpdateSoloCerts')->daily();
-        $schedule->command('RosterUpdate:UpdateRoster')->hourly();
-        $schedule->command('VATUSAEvents:Update')->hourly();
-        $schedule->command('VATUSATrainingTickets:UploadPending')->hourly();
-        $schedule->command('Overflights:GetOverflights')->everyFiveMinutes();
+        $schedule->command('cache:prune-stale-tags')->hourlyAt(3);
+        $schedule->command('SoloCerts:UpdateSoloCerts')->dailyAt('05:01');
+        $schedule->command('RosterUpdate:UpdateRoster')->hourlyAt(7);
+        $schedule->command('VATUSAEvents:Update')->hourlyAt(22);
+        $schedule->command('VATUSATrainingTickets:UploadPending')->hourlyAt(33);
+        $schedule->command('Overflights:GetOverflights')->everyFourMinutes();
         $schedule->command('Weather:UpdateWeather')->everyFiveMinutes();
-        if (FeatureToggle::isEnabled('online_controller_debug')) {
-            $schedule->command('OnlineControllers:GetControllers')->everyMinute()->appendOutputTo('storage/logs/online-controllers.log')->emailOutputOnFailure('wm@ztlartcc.org');
-        } else {
-            $schedule->command('OnlineControllers:GetControllers')->everyMinute();
-        }
+        $schedule->command('OnlineControllers:GetControllers')->everyMinute();
         if (FeatureToggle::isEnabled("auto_support_events")) {
-            $schedule->command('Events:UpdateSupportEvents')->daily();
+            $schedule->command('Events:UpdateSupportEvents')->daily()->dailyAt('05:09');
         }
     }
 
