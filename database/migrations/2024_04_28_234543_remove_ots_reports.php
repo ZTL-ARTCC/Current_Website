d031dd1c5b483e68f57aaca05fe1f2af75575eef
Evanston09 <evanhkim09@gmail.com>
2024-04-28 19:57:18 -0400

    Fix formatting

--- database/migrations/2024_04_28_234543_remove_ots_reports.php
+++ database/migrations/2024_04_28_234543_remove_ots_reports.php
@@ -1,17 +1,14 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('ots_recommendations', function($table) {
+    public function up(): void {
+        Schema::table('ots_recommendations', function ($table) {
             $table->dropColumn('report');
         });
     }
@@ -19,9 +16,8 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::table('ots_recommendations', function($table) {
+    public function down(): void {
+        Schema::table('ots_recommendations', function ($table) {
             $table->text('report')->nullable();
         });
     }
