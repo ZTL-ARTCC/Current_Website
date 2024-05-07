44fd8c7aa2959b8990dc0ad25b67b14b3cb7908e
kjporter <kporter@kplink.net>
2024-05-07 18:20:25 -0400

    Update RealopsController.php

--- app/Http/Controllers/RealopsController.php
+++ app/Http/Controllers/RealopsController.php
@@ -46,7 +46,7 @@ class RealopsController extends Controller {
             return redirect()->back()->with('error', 'That flight doesn\'t exist');
         }
 
-        if ( $flight->assigned_pilot_id ) {
+        if ($flight->assigned_pilot_id) {
             return redirect()->back()->with('error', 'That flight already has a pilot assigned');
         }
         
