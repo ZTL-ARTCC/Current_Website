aba7557526d08f3a605c1129a1d6a6135815be8d
kjporter <kporter@kplink.net>
2024-07-15 20:14:56 -0400

    Update TrainingReminderEmails.php

--- app/Console/Commands/TrainingReminderEmails.php
+++ app/Console/Commands/TrainingReminderEmails.php
@@ -45,15 +45,28 @@ class TrainingReminderEmails extends Command {
                 ->join('ea_users AS customer_user', 'ea_appointments.id_users_customer', '=', 'customer_user.id')
                 ->join('ea_users AS staff_user', 'ea_appointments.id_users_provider', '=', 'staff_user.id')
                 ->select('customer_user.first_name AS first_name', 'customer_user.last_name AS last_name', 'customer_user.email AS email', 'ea_appointments.start_datetime AS start_datetime', 'ea_services.name AS service_description', 'staff_user.first_name AS staff_first_name', 'staff_user.last_name AS staff_last_name')
-                ->where('ea_appointments.start_datetime', '>', Carbon::now('America/New_York')->subHours(23)->format('Y-m-d H:i:s'))
+                ->where('ea_appointments.start_datetime', '>=', Carbon::now('America/New_York')->subHours(23)->format('Y-m-d H:i:s'))
                 ->where('ea_appointments.start_datetime', '<', Carbon::now('America/New_York')->subHours(24)->format('Y-m-d H:i:s'))
                 ->get();
             foreach ($ea_appointments as $ea_appointment) {
-                $this->info('Sending email reminder to ' . $ea_appointment->first_name . ' ' . $ea_appointment->last_name . ' for ' . $ea_appointment->service_description . ' with ' . $ea_appointment->staff_first_name . ' ' . $ea_appointment->staff_last_name . ' on ' . Carbon::parse($ea_appointment->start_datetime)->format('m/d/Y') . ' ' . Carbon::parse($ea_appointment->start_datetime)->format('H:i') . ' ET');
-                Mail::send('emails.training_reminder', ['trainee_name' => $ea_appointment->first_name . ' ' . $ea_appointment->last_name, 'session_type' => $ea_appointment->service_description, 'mentor_name' => $ea_appointment->staff_first_name . ' ' . $ea_appointment->staff_last_name, 'session_date_time' => Carbon::parse($ea_appointment->start_datetime)->format('m/d/Y') . ' ' . Carbon::parse($ea_appointment->start_datetime)->format('H:i') . ' ET'], function ($message) use ($ea_appointment) {
-                    $message->from('training@notams.ztlartcc.org', 'vZTL ARTCC Training Department')->subject('Reminder: ' . $ea_appointment->service_description . ' with ' . $ea_appointment->staff_first_name . ' ' . $ea_appointment->staff_last_name . ' on ' . Carbon::parse($ea_appointment->start_datetime)->format('m/d/Y') . ' ' . Carbon::parse($ea_appointment->start_datetime)->format('H:i') . ' ET');
-                    $message->to($ea_appointment->email);
-                });
+                $student_full_name = $ea_appointment->first_name . ' ' . $ea_appointment->last_name;
+                $trainer_full_name = $ea_appointment->staff_first_name . ' ' . $ea_appointment->staff_last_name;
+                $appointment_date_time = Carbon::parse($ea_appointment->start_datetime)->format('m/d/Y') . ' ' . Carbon::parse($ea_appointment->start_datetime)->format('H:i') . ' ET';
+                $this->info('Sending email reminder to ' . $student_full_name . ' for ' . $ea_appointment->service_description . ' with ' . $trainer_full_name . ' on ' . $appointment_date_time);
+                Mail::send(
+                    'emails.training_reminder',
+                    [
+                        'trainee_name' => $student_full_name,
+                        'session_type' => $ea_appointment->service_description,
+                        'mentor_name' => $trainer_full_name,
+                        'session_date_time' => $appointment_date_time
+                    ],
+                    function ($message) use ($ea_appointment, $trainer_full_name, $appointment_date_time) {
+                        $message->from('training@notams.ztlartcc.org', 'vZTL ARTCC Training Department')
+                            ->subject('Reminder: ' . $ea_appointment->service_description . ' with ' . $trainer_full_name . ' on ' . $appointment_date_time)
+                            ->to($ea_appointment->email);
+                    }
+                );
             }
         } catch (\Illuminate\Database\QueryException $e) {
             $this->info('Error: ' . $e);
