d4d48639e10163d68de46520bb90524170f0f639
Evanston09 <evanhkim09@gmail.com>
2024-07-19 19:00:23 -0400

    Change migration and create model

---
+++ app/EventDenylist.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventDenylist extends Model
+{
+    protected $table = 'event_denylists';
+    protected $fillable = ['id', 'vatsim_id', 'created_at', 'updated_at'];
+}
