f34c40011b26c7f2de1c417d8b4217ea06df9da6
Evanston09 <evanhkim09@gmail.com>
2024-07-19 17:38:01 -0400

    Create migration

---
+++ database/migrations/2024_07_19_213434_create_event_denylist_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_denylist', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('vatsim_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_denylist');
+    }
+};
