50c6641bfc7c2c7f3706cf43f1659e37cc8a248f
Evanston09 <evanhkim09@gmail.com>
2024-07-24 11:01:24 -0400

    Fix JS

--- resources/assets/js/mix/event_index.js
+++ resources/assets/js/mix/event_index.js
@@ -1,8 +1,11 @@
-$('#denylistEvent').on('show.bs.modal', function(e) {
-    var eventId = $(e.relatedTarget).data('id');
-    var deleteLink = $(this).find('#deleteLink');
-    var denylistLink = $(this).find('#denylistLink');
+$("#denylistEvent").on("show.bs.modal", function (e) {
+  var eventId = $(e.relatedTarget).data("id");
+  var deleteLink = $(this).find("#deleteLink");
+  var denylistLink = $(this).find("#denylistLink");
 
-    deleteLink.attr('href', '/dashboard/admin/events/delete/' + eventId);
-    denylistLink.attr('href', '/dashboard/admin/events/delete/' + eventId + '?denylist=true');
+  deleteLink.attr("href", "/dashboard/admin/events/delete/" + eventId);
+  denylistLink.attr(
+    "href",
+    "/dashboard/admin/events/delete/" + eventId + "?denylist=true"
+  );
 });
