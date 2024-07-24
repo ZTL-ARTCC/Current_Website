65538a582d0cdd77301edc751d662f2e773124c4
kjporter <kporter@kplink.net>
2024-07-24 14:26:30 -0400

    Fix copy-to-clipboard

--- resources/assets/js/mix/copy_to_clipboard.js
+++ resources/assets/js/mix/copy_to_clipboard.js
@@ -7,3 +7,26 @@ function copyTextToClipboard(text) {
 }
 
 window.copyTextToClipboard = copyTextToClipboard;
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    document.execCommand("copy");
+  } catch (err) {
+    //Do nothing
+  }
+  document.body.removeChild(textArea);
+}
+
+window.fallbackCopyTextToClipboard = fallbackCopyTextToClipboard;
