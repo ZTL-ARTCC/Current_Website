b1a60be3b44979a1c37403d8395d27f1ca20b71d
SpectreLoki <98547490+SpectreLoki@users.noreply.github.com>
2024-08-24 17:05:27 +0200

    Fix bracket bug in Edit Calendar

--- resources/views/dashboard/admin/calendar/edit.blade.php
+++ resources/views/dashboard/admin/calendar/edit.blade.php
@@ -38,7 +38,7 @@ Edit Calendar Event/News
     </div>
     <div class="form-group">
         <label for="body">Additional Information</label>
-        {{ html()->textarea('body', $calendar->body)->class(['form-control', 'text-editor')->placeholder('Required') }}
+        {{ html()->textarea('body', $calendar->body)->class(['form-control', 'text-editor'])->placeholder('Required') }}
     </div>
     <div class="form-group">
         <label for="type">Type of Post</label>
