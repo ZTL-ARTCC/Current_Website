b4e0fbddeff7e179bf387aafb67271a0a7ba142f
Ian Cowan <ian@cowan.aero>
2024-09-07 12:18:42 -0600

    #580 Refactor training ticket emails for async

--- app/Http/Controllers/TrainingDash.php
+++ app/Http/Controllers/TrainingDash.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Audit;
 use App\Mail\OtsAssignment;
+use App\Mail\TrainingTicketMail;
 use App\Ots;
 use App\PublicTrainingInfo;
 use App\PublicTrainingInfoPdf;
@@ -13,10 +14,8 @@ use App\User;
 use Auth;
 use Carbon\Carbon;
 use Config;
-use Exception;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
 use Mail;
 use mitoteam\jpgraph\MtJpGraph;
@@ -779,15 +778,7 @@ class TrainingDash extends Controller {
         $controller = User::find($ticket->controller_id);
         $trainer = User::find($ticket->trainer_id);
 
-        try {
-            Mail::send(['html' => 'emails.training_ticket'], ['ticket' => $ticket, 'controller' => $controller, 'trainer' => $trainer], function ($m) use ($controller, $ticket) {
-                $m->from('training@notams.ztlartcc.org', 'vZTL ARTCC Training Department');
-                $m->subject('New Training Ticket Submitted');
-                $m->to($controller->email)->cc('training@ztlartcc.org');
-            });
-        } catch (Exception $e) {
-            Log::info('Unable to send training ticket email: ' . $e);
-        }
+        Mail::to($controller->email)->cc('training@ztlartcc.org')->send(new TrainingTicketMail($ticket, $controller, $trainer));
 
         if ($request->ots == 1) {
             $ots = new Ots;
