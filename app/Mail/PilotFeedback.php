d1666fde13af59e4fa6e08049394350c332f53ba
Ian Cowan <ian@cowan.aero>
2024-09-07 18:34:28 -0600

    #580 Refactor pilot feedback email for async

---
+++ app/Mail/PilotFeedback.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Mail;
+
+use App\Mail\Package\ZTLAddress;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PilotFeedback extends Mailable implements ShouldQueue {
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public $feedback, public $subject, public $body, public $sender, public $replyToAddress, public $replyToName) {
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope {
+        return new Envelope(
+            from: new ZTLAddress('feedback', 'vZTL ARTCC Feedback Department'),
+            replyTo: [
+                new Address($this->replyToAddress, $this->replyToName)
+            ],
+            subject: $this->subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content {
+        return new Content(
+            view: 'emails.feedback_email',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array {
+        return [];
+    }
+}
