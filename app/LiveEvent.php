874a060944f91c4711454cbc254d3bd294b2a2ba
kjporter <kporter@kplink.net>
2024-09-17 12:04:04 -0400

    Adds helper functions

--- app/LiveEvent.php
+++ app/LiveEvent.php
@@ -6,4 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class LiveEvent extends Model {
     protected $table = 'live_event';
+
+    public function getStaffNameAttribute() {
+        $editor = User::find($this->staff_member);
+        if (is_null($editor)) {
+            return 'Unknown';
+        }
+        return $editor->full_name;
+    }
+
+    public function getUpdateTimeAttribute() {
+        $date = $this->updated_at;
+        if (is_null($date)) {
+            return 'Never';
+        }
+        return $date->format('D M d, Y').' at '.substr($date, 11).'z';
+    }
 }
