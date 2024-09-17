3bb4bb1d09b29649e1be4b7b5e69bf004b4ec8cc
kjporter <kporter@kplink.net>
2024-09-17 12:04:19 -0400

    Updates defaults and add seeding

--- database/migrations/2024_09_13_160030_live_event_info.php
+++ database/migrations/2024_09_13_160030_live_event_info.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -14,8 +15,8 @@ return new class extends Migration {
         Schema::create('live_event', function (Blueprint $table) {
             $table->increments('id');
             $table->string('event_title');
-            $table->text('body_public')->nullable();
-            $table->text('body_private')->nullable();
+            $table->text('body_public');
+            $table->text('body_private');
             $table->integer('staff_member');
             $table->timestamps();
         });
