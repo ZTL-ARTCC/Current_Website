861b96c1ca9232d435cfe1a18e0b984dab004a96
kjporter <kporter@kplink.net>
2024-09-22 20:50:54 -0400

    Update trainer_feedback.blade.php

--- resources/views/inc/trainer_feedback.blade.php
+++ resources/views/inc/trainer_feedback.blade.php
@@ -2,11 +2,11 @@
     {{ html()->form()->route('saveNewTrainerFeedback')->open() }}
         <div class="row">
             <div class="col-sm-4 form-group">
-                <label for="feedback_id" class="control-label">Training Team Member:</label>
+                <label for="feedback_id" class="control-label">Training Team Member: (required)</label>
                 {{ html()->select('feedback_id', $feedbackOptions)->placeholder('Select Team Member')->class(['form-control']) }}
             </div>
             <div class="col-sm-4 form-group">
-                <label for="feedback_date" class="control-label">Date of Session/Event:</label>
+                <label for="feedback_date" class="control-label">Date of Session/Event: (required)</label>
                 <div class="input-group date dt_picker_date" id="datetimepicker1" data-target-input="nearest">
                         {{ html()->text('feedback_date', null)->placeholder('MM/DD/YYYY')->class(['form-control', 'datetimepicker-input'])->attributes(['data-target' => '#datetimepicker1']) }}
                         <div class="input-group-append" data-target="#datetimepicker1" data-toggle="datetimepicker">
@@ -15,7 +15,7 @@
                     </div>
             </div>
             <div class="col-sm-4 form-group">
-                <label for="service_level" class="control-label">Service Level:</label>
+                <label for="service_level" class="control-label">Service Level: (required)</label>
                 <div id="stars" class="star-input input-group">
                         <span data-rating="1">&star;</span>
                         <span data-rating="2">&star;</span>
@@ -28,21 +28,21 @@
         </div>
         <div class="row">
             <div class="col-sm-4 form-group">
-                <label for="position_trained" class="control-label">Position Trained/Training Session ID:</label>
+                <label for="position_trained" class="control-label">Position Trained/Training Session ID: (required)</label>
                 {{ html()->text('position_trained', null)->placeholder('Position Staffed')->class(['form-control']) }}
             </div>
             <div class="col-sm-4 form-group">
-                <label for="booking_method" class="control-label">Booking Method:</label>
+                <label for="booking_method" class="control-label">Booking Method: (required)</label>
                 {{ html()->select('booking_method', [0=>'Easy!Appointments', 1=>'Ad-Hoc'])->placeholder('Pick One')->class(['form-control']) }}
             </div>
             <div class="col-sm-4 form-group">
-                <label for="training_method" class="control-label">Training Method:</label>
+                <label for="training_method" class="control-label">Training Method: (required)</label>
                 {{ html()->select('training_method', [0=>'Theory', 1=>'Sweatbox', 2=>'Live Network'])->placeholder('Pick One')->class(['form-control']) }}
             </div>
         </div>
         <div class="row form-group">
             <div class="col-sm-12 form-group">
-                <label for="comments" class="control-label">Comments:</label>
+                <label for="comments" class="control-label">Comments: (required)</label>
                 {{ html()->textarea('comments', null)->placeholder('Type your comments here. This will be reviewed by the facility TA and ATA.')->class(['form-control']) }}
             </div>
         </div>
@@ -53,11 +53,11 @@
             </div>
             <div class="col-sm-4 form-group">
                 <label for="student_email" class="control-label">Your Email: (optional)</label>
-                {{ html()->email('pilot_email', null)->placeholder('Your Email')->class(['form-control']) }}
+                {{ html()->email('student_email', null)->placeholder('Your Email')->class(['form-control']) }}
             </div>
             <div class="col-sm-4 form-group">
                 <label for="student_cid" class="control-label">Your VATSIM CID: (optional)</label>
-                {{ html()->text('pilot_cid', null)->placeholder('Your VATSIM CID')->class(['form-control']) }}
+                {{ html()->text('student_cid', null)->placeholder('Your VATSIM CID')->class(['form-control']) }}
             </div>
         </div>
         <div class="row">
