006e871ccb04710feaf6ffa11c5241d8c09c5080
kjporter <kporter@kplink.net>
2024-11-10 14:23:52 -0500

    Update enroll.blade.php

--- resources/views/emails/pilot_passport/enroll.blade.php
+++ resources/views/emails/pilot_passport/enroll.blade.php
@@ -7,8 +7,8 @@
     our website at <a href="https://www.ztlartcc.org/pilot_passport" alt="Website">https://www.ztlartcc.org/pilot_passport</a>.</p>
 
 <p>Please remember that in order to get credit for visiting a participating airfield, you should plan to land and remain on the ground for 
-    about 5 minutes. You can immediately verify that we logged your visit by refreshing your passport book on our website or by monitoring
-    your email (we will send a confirmation email with each qualifying visit).</p>
+    about 5 minutes. You can immediately verify that we logged your visit by refreshing your passport book on our website. We will also send an
+    email confirmation with each qualifying visit (emails may be delayed up to 5 minutes).</p>
 
 <p>If you did not enroll in this program or otherwise believe this email to be in error, please contact us at: 
     <a href="emailto:wm@ztlartcc.org" alt="Email">wm@ztlartcc.org</a>.</p>
