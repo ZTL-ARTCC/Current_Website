11dadcbfa57e72e974f85daae09b8351ed1ef3c7
Ian Cowan <ian@cowan.aero>
2024-11-29 09:04:18 -0600

    #698 Show empty state on public pages for empty tables

--- resources/views/site/airports/index.blade.php
+++ resources/views/site/airports/index.blade.php
@@ -24,21 +24,25 @@ Airports
         </div>
     </div>
     <br>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Airport Name</th>
-                <th scope="col">Latest METAR</th>
-                <th scope="col">Visual Conditions</th>
-            </tr>
-            @foreach($airports as $a)
+    @if(count($airports) > 0)
+        <table class="table">
+            <thead>
                 <tr>
-                    <td><a href="/pilots/airports/view/{{ $a->id }}">{{ $a->name }} ({{ $a->ltr_3 }})</a></td>
-                    <td>{{ $a->metar }}</td>
-                    <td>{{ $a->visual_conditions }}</td>
+                    <th scope="col">Airport Name</th>
+                    <th scope="col">Latest METAR</th>
+                    <th scope="col">Visual Conditions</th>
                 </tr>
-            @endforeach
-        </thead>
-    </table>
+                @foreach($airports as $a)
+                    <tr>
+                        <td><a href="/pilots/airports/view/{{ $a->id }}">{{ $a->name }} ({{ $a->ltr_3 }})</a></td>
+                        <td>{{ $a->metar }}</td>
+                        <td>{{ $a->visual_conditions }}</td>
+                    </tr>
+                @endforeach
+            </thead>
+        </table>
+    @else
+        @include('inc.empty_state', ['header' => 'No Airports', 'body' => 'There are no airports to display matching that search criteria.', 'icon' => 'fa fa-plane'])
+    @endif
 </div>
 @endsection
