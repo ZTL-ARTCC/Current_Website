cbf371134afc87f1b117aba676927628cc543346
Ian Cowan <ian@cowan.aero>
2025-01-06 14:38:28 -0500

    #698 Empty state for calendar and news management

--- resources/views/dashboard/admin/calendar/index.blade.php
+++ resources/views/dashboard/admin/calendar/index.blade.php
@@ -20,104 +20,104 @@ Calendar/News
     </ul>
     <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="calendar">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Date/Time</th>
-                        <th scope="col"><center>Title</center></th>
-                        <th scope="col"><center>Body</center></th>
-                        <th scope="col"><center>Created By</center></th>
-                        <th scope="col"><center>Last Updated By</center></th>
-                        <th scope="col" style="width:110px"><center>Actions</center></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if(count($calendar) > 0)
-                        @foreach($calendar as $c)
-                            <tr>
-                                @if($c->time == null)
-                                    <td>{{ $c->date }}</td>
-                                @else
-                                    <td>{{ $c->date }} {{ $c->time }}</td>
-                                @endif
-                                <td><center><a href="/dashboard/admin/calendar/view/{{ $c->id }}">{{ $c->title }}</a></center></td>
-                                <td><center>{!! str_limit($c->body, 50, '...') !!}</center></td>
-                                <td data-toggle="tooltip" title="{{ $c->created_at }}"><center>{{ App\User::find($c->created_by)->full_name }}</center></td>
-                                @if($c->updated_by != null)
-                                    <td data-toggle="tooltip" title="{{ $c->updated_at }}"><center>{{ App\User::find($c->updated_by)->full_name }}</center></td>
-                                @else
-                                    <td></td>
-                                @endif
-                                <td>
-                                    {{ html()->form('DELETE')->route('deleteCalendarEvent', [$c->id])->open() }}
-                                        @csrf
-                                        <a class="btn btn-success simple-tooltip" href="/dashboard/admin/calendar/edit/{{ $c->id }}" data-toggle="tooltip" title="Edit"><i class="far fa-edit"></i></a>
-                                        <button class="btn btn-danger simple-tooltip" type="submit" data-toggle="tooltip" title="Delete"><i class="fa fa-times"></i></button>
-                                    {{ html()->form()->close() }}
-                                </td>
-                            </tr>
-                        @endforeach
-                    @else
-                        <p>No calendar events to show.</p>
-                    @endif
+            @if(count($calendar) > 0)
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Date/Time</th>
+                            <th scope="col"><center>Title</center></th>
+                            <th scope="col"><center>Body</center></th>
+                            <th scope="col"><center>Created By</center></th>
+                            <th scope="col"><center>Last Updated By</center></th>
+                            <th scope="col" style="width:110px"><center>Actions</center></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($calendar as $c)
+                        <tr>
+                            @if($c->time == null)
+                                <td>{{ $c->date }}</td>
+                            @else
+                                <td>{{ $c->date }} {{ $c->time }}</td>
+                            @endif
+                            <td><center><a href="/dashboard/admin/calendar/view/{{ $c->id }}">{{ $c->title }}</a></center></td>
+                            <td><center>{!! str_limit($c->body, 50, '...') !!}</center></td>
+                            <td data-toggle="tooltip" title="{{ $c->created_at }}"><center>{{ App\User::find($c->created_by)->full_name }}</center></td>
+                            @if($c->updated_by != null)
+                                <td data-toggle="tooltip" title="{{ $c->updated_at }}"><center>{{ App\User::find($c->updated_by)->full_name }}</center></td>
+                            @else
+                                <td></td>
+                            @endif
+                            <td>
+                                {{ html()->form('DELETE')->route('deleteCalendarEvent', [$c->id])->open() }}
+                                    @csrf
+                                    <a class="btn btn-success simple-tooltip" href="/dashboard/admin/calendar/edit/{{ $c->id }}" data-toggle="tooltip" title="Edit"><i class="far fa-edit"></i></a>
+                                    <button class="btn btn-danger simple-tooltip" type="submit" data-toggle="tooltip" title="Delete"><i class="fa fa-times"></i></button>
+                                {{ html()->form()->close() }}
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
+            @else
+                @include('inc.empty_state', ['header' => 'No Calendar Events to Show', 'body' => 'There are no calendar events to show', 'icon' => 'fa-solid fa-calendar'])
+            @endif
         </div>
 
         <div role="tabpanel" class="tab-pane" id="news">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col"><center>Title</center></th>
-                        <th scope="col"><center>Body</center></th>
-                        <th scope="col"><center>Created By</center></th>
-                        <th scope="col"><center>Last Updated By</center></th>
-                        <th scope="col" style="width:110px"><center>Actions</center></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if(count($news) > 0)
-                        @foreach($news as $c)
-                            <tr>
-                                @if($c->time == null)
-                                    <td>{{ $c->date }}</td>
-                                @else
-                                    <td>{{ $c->date }} {{ $c->time }}</td>
-                                @endif
-                                <td><center><a href="/dashboard/admin/calendar/view/{{ $c->id }}">{{ $c->title }}</a></center></td>
-                                <td><center>{!! str_limit($c->body, 50, '...') !!}</center></td>
-                                <td data-toggle="tooltip" title="{{ $c->created_at }}"><center>{{ App\User::find($c->created_by)->full_name }}</center></td>
-                                @if($c->updated_by != null)
-                                    <td data-toggle="tooltip" title="{{ $c->updated_at }}"><center>{{ App\User::find($c->updated_by)->full_name }}</center></td>
-                                @else
-                                    <td></td>
-                                @endif
-                                <td>
-                                    {{ html()->form('DELETE')->route('deleteCalendarEvent', [$c->id])->open() }}
-                                        @csrf
-                                        <a class="btn btn-success simple-tooltip" href="/dashboard/admin/calendar/edit/{{ $c->id }}" data-toggle="tooltip" title="Edit"><i class="far fa-edit"></i></a>
-                                        <button class="btn btn-danger simple-tooltip" type="submit" data-toggle="tooltip" title="Delete"><i class="fa fa-times"></i></button>
-                                    {{ html()->form()->close() }}
-                                </td>
-
-                                <td>
-                                    {{ html()->form()->route('toggleCalendarEventVisibility', [$c->id])->open() }}
+            @if(count($news) > 0)
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Date</th>
+                            <th scope="col"><center>Title</center></th>
+                            <th scope="col"><center>Body</center></th>
+                            <th scope="col"><center>Created By</center></th>
+                            <th scope="col"><center>Last Updated By</center></th>
+                            <th scope="col" style="width:110px"><center>Actions</center></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($news as $c)
+                        <tr>
+                            @if($c->time == null)
+                                <td>{{ $c->date }}</td>
+                            @else
+                                <td>{{ $c->date }} {{ $c->time }}</td>
+                            @endif
+                            <td><center><a href="/dashboard/admin/calendar/view/{{ $c->id }}">{{ $c->title }}</a></center></td>
+                            <td><center>{!! str_limit($c->body, 50, '...') !!}</center></td>
+                            <td data-toggle="tooltip" title="{{ $c->created_at }}"><center>{{ App\User::find($c->created_by)->full_name }}</center></td>
+                            @if($c->updated_by != null)
+                                <td data-toggle="tooltip" title="{{ $c->updated_at }}"><center>{{ App\User::find($c->updated_by)->full_name }}</center></td>
+                            @else
+                                <td></td>
+                            @endif
+                            <td>
+                                {{ html()->form('DELETE')->route('deleteCalendarEvent', [$c->id])->open() }}
                                     @csrf
-                                    @if($c->visible == 0)
-                                        <button class="btn btn-success" data-toggle="tooltip" title="Show News"><i class="far fa-eye"></i></button>
-                                    @elseif($c->visible == 1)
-                                        <button class="btn btn-warning" data-toggle="tooltip" title="Hide News"><i class="fas fa-ban"></i></button>
-                                    @endif
-                                    {{ html()->form()->close() }}
-                                </td>
-                            </tr>
-                        @endforeach
-                    @else
-                        <p>No news to show.</p>
-                    @endif
+                                    <a class="btn btn-success simple-tooltip" href="/dashboard/admin/calendar/edit/{{ $c->id }}" data-toggle="tooltip" title="Edit"><i class="far fa-edit"></i></a>
+                                    <button class="btn btn-danger simple-tooltip" type="submit" data-toggle="tooltip" title="Delete"><i class="fa fa-times"></i></button>
+                                {{ html()->form()->close() }}
+                            </td>
+
+                            <td>
+                                {{ html()->form()->route('toggleCalendarEventVisibility', [$c->id])->open() }}
+                                @csrf
+                                @if($c->visible == 0)
+                                    <button class="btn btn-success" data-toggle="tooltip" title="Show News"><i class="far fa-eye"></i></button>
+                                @elseif($c->visible == 1)
+                                    <button class="btn btn-warning" data-toggle="tooltip" title="Hide News"><i class="fas fa-ban"></i></button>
+                                @endif
+                                {{ html()->form()->close() }}
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
+            @else
+                @include('inc.empty_state', ['header' => 'No News to Show', 'body' => 'There is no news to show', 'icon' => 'fa-solid fa-newspaper'])
+            @endif
         </div>
     </div>
 </div>
