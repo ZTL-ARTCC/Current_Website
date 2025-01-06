8b1f6dc088ad919ecb79e8436a9be74c2ec11460
Ian Cowan <ian@cowan.aero>
2025-01-06 14:48:11 -0500

    #698 Empty state for airport management

--- resources/views/dashboard/admin/airports/index.blade.php
+++ resources/views/dashboard/admin/airports/index.blade.php
@@ -10,37 +10,41 @@ Airport Management
 <div class="container">
     <a href="/dashboard/admin/airports/new" class="btn btn-primary">New Airport</a>
     <br><br>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Airport Name</th>
-                <th scope="col">ICAO Identifier</th>
-                <th scope="col">Actions</th>
-            </tr>
-            @foreach($airports as $a)
+    @if($airports->count() > 0)
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{ $a->name }}</td>
-                    <td>{{ $a->ltr_4 }}</td>
-                    <td>
-                        <div class="row">
-                            <div class="col-sm-2">
-                                @if($a->front_pg == 1)
-                                    <a href="/dashboard/admin/airports/del-from-home/{{ $a->id }}" class="btn btn-danger simple-tooltip" data-toggle="tooltip" title="Remove from Home Page"><i class="fas fa-minus"></i></a>
-                                @else
-                                    <a href="/dashboard/admin/airports/add-to-home/{{ $a->id }}" class="btn btn-success simple-tooltip" data-toggle="tooltip" title="Add to Home Page"><i class="fas fa-plus"></i></a>
-                                @endif
-                            </div>
-                            <div class="col-sm-2">
-                                {{ html()->form('DELETE')->route('deleteAirport', [$a->id])->open() }}
-                                    @csrf
-                                    <button class="btn btn-danger simple-tooltip" action="submit" data-toggle="tooltip" title="Delete Airport"><i class="fas fa-times"></i></button>
-                                {{ html()->form()->close() }}
-                            </div>
-                        </div>
-                    </td>
+                    <th scope="col">Airport Name</th>
+                    <th scope="col">ICAO Identifier</th>
+                    <th scope="col">Actions</th>
                 </tr>
-            @endforeach
-        </thead>
-    </table>
+                @foreach($airports as $a)
+                    <tr>
+                        <td>{{ $a->name }}</td>
+                        <td>{{ $a->ltr_4 }}</td>
+                        <td>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    @if($a->front_pg == 1)
+                                        <a href="/dashboard/admin/airports/del-from-home/{{ $a->id }}" class="btn btn-danger simple-tooltip" data-toggle="tooltip" title="Remove from Home Page"><i class="fas fa-minus"></i></a>
+                                    @else
+                                        <a href="/dashboard/admin/airports/add-to-home/{{ $a->id }}" class="btn btn-success simple-tooltip" data-toggle="tooltip" title="Add to Home Page"><i class="fas fa-plus"></i></a>
+                                    @endif
+                                </div>
+                                <div class="col-sm-2">
+                                    {{ html()->form('DELETE')->route('deleteAirport', [$a->id])->open() }}
+                                        @csrf
+                                        <button class="btn btn-danger simple-tooltip" action="submit" data-toggle="tooltip" title="Delete Airport"><i class="fas fa-times"></i></button>
+                                    {{ html()->form()->close() }}
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </thead>
+        </table>
+    @else
+        @include('inc.empty_state', ['header' => 'No Airports', 'body' => 'There are no airports to show', 'icon' => 'fa fa-tower-observation'])
+    @endif
 </div>
 @endsection
