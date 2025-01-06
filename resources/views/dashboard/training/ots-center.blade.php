4c455a19966ce2434cb93b9a2dbc5ac057fe24c9
Ian Cowan <ian@cowan.aero>
2025-01-06 14:27:21 -0500

    #698 Empty state for OTS center

--- resources/views/dashboard/training/ots-center.blade.php
+++ resources/views/dashboard/training/ots-center.blade.php
@@ -23,126 +23,122 @@ OTS Center
     </ul>
     <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="new">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Date of Recommendation</th>
-                        <th scope="col">Controller Name</th>
-                        <th scope="col">Recommender Name</th>
-                        <th scope="col">Position</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                    @if($ots_new->count() > 0)
-                        @foreach($ots_new as $o)
-                            <tr>
-                                <td>{{ $o->recommended_on }}</td>
-                                <td>{{ $o->controller_name }}</td>
-                                <td>{{ $o->recommender_name }}</td>
-                                <td>{{ $o->position_name }}</td>
-                                <td>
-                                    <a href="/dashboard/training/ots-center/accept/{{ $o->id }}" class="btn btn-success simple-tooltip" data-toggle="tooltip" title="Accept Recommendation"><i class="fas fa-check"></i></a>
-                                    <a href="/dashboard/training/tickets?id={{ $o->controller_id }}" class="btn btn-info simple-tooltip" data-toggle="tooltip" title="View Training Tickets."><i class="fas fa-check"></i></a>
-                                    @if(Auth::user()->isAbleTo('snrStaff'))
-                                        <a href="/dashboard/training/ots-center/reject/{{ $o->id }}" class="btn btn-danger simple-tooltip" data-toggle="tooltip" title="Reject Recommendation"><i class="fas fa-times"></i></a>
-                                        <span data-toggle="modal" data-target="#assign{{ $o->id }}">
-                                            <button type="button" class="btn btn-success simple-tooltip" data-placement="top" data-toggle="tooltip" title="Assign to Instructor"><i class="fas fa-user-check"></i></button>
-                                        </span>
-                                    @endif
-                                </td>
-                            </tr>
-                            <div class="modal fade" id="assign{{ $o->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title">Assign OTS for {{ $o->controller_name }} to Instructor</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        {{ html()->form()->route('assignRecommendation', [$o->id])->open() }}
-                                        @csrf
-                                        <div class="modal-body">
-                                        {{ html()->select('ins', $instructors, null)->placeholder('Select Instructor')->class(['form-control']) }}
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                            <button action="submit" class="btn btn-success">Assign</button>
-                                        </div>
-                                        {{ html()->form()->close() }}
+            @if($ots_new->count() > 0)
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Date of Recommendation</th>
+                            <th scope="col">Controller Name</th>
+                            <th scope="col">Recommender Name</th>
+                            <th scope="col">Position</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    @foreach($ots_new as $o)
+                        <tr>
+                            <td>{{ $o->recommended_on }}</td>
+                            <td>{{ $o->controller_name }}</td>
+                            <td>{{ $o->recommender_name }}</td>
+                            <td>{{ $o->position_name }}</td>
+                            <td>
+                                <a href="/dashboard/training/ots-center/accept/{{ $o->id }}" class="btn btn-success simple-tooltip" data-toggle="tooltip" title="Accept Recommendation"><i class="fas fa-check"></i></a>
+                                <a href="/dashboard/training/tickets?id={{ $o->controller_id }}" class="btn btn-info simple-tooltip" data-toggle="tooltip" title="View Training Tickets."><i class="fas fa-check"></i></a>
+                                @if(Auth::user()->isAbleTo('snrStaff'))
+                                    <a href="/dashboard/training/ots-center/reject/{{ $o->id }}" class="btn btn-danger simple-tooltip" data-toggle="tooltip" title="Reject Recommendation"><i class="fas fa-times"></i></a>
+                                    <span data-toggle="modal" data-target="#assign{{ $o->id }}">
+                                        <button type="button" class="btn btn-success simple-tooltip" data-placement="top" data-toggle="tooltip" title="Assign to Instructor"><i class="fas fa-user-check"></i></button>
+                                    </span>
+                                @endif
+                            </td>
+                        </tr>
+                        <div class="modal fade" id="assign{{ $o->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Assign OTS for {{ $o->controller_name }} to Instructor</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
                                     </div>
+                                    {{ html()->form()->route('assignRecommendation', [$o->id])->open() }}
+                                    @csrf
+                                    <div class="modal-body">
+                                    {{ html()->select('ins', $instructors, null)->placeholder('Select Instructor')->class(['form-control']) }}
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                        <button action="submit" class="btn btn-success">Assign</button>
+                                    </div>
+                                    {{ html()->form()->close() }}
                                 </div>
                             </div>
                         </div>
-                        @endforeach
-                    @else
-                        <tr>
-                            <td colspan="6">No new OTS exam recommendations found.</td>
-                        </tr>
-                    @endif
+                    </div>
+                    @endforeach
                 </thead>
             </table>
+            @else
+                @include('inc.empty_state', ['header' => 'No New Recommendations', 'body' => 'No new OTS recommendations at this time', 'icon' => 'fa-solid fa-file'])
+            @endif
         </div>
         <div role="tabpanel" class="tab-pane" id="accepted">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Date of Recommendation</th>
-                        <th scope="col">Controller Name</th>
-                        <th scope="col">Recommender Name</th>
-                        <th scope="col">Instructor</th>
-                        <th scope="col">Position</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                    @if($ots_accepted->count() > 0)
-                        @foreach($ots_accepted as $o)
-                            <tr>
-                                <td>{{ $o->recommended_on }}</td>
-                                <td>{{ $o->controller_name }}</td>
-                                <td>{{ $o->recommender_name }}</td>
-                                <td>{{ $o->ins_name }}</td>
-                                <td>{{ $o->position_name }}</td>
-                                <td>
-                                    @if($o->ins_id == Auth::id() || Auth::user()->isAbleTo('snrStaff'))
-                                        <span data-toggle="modal" data-target="#completeOTS{{ $o->id }}">
-                                            <button type="button" class="btn btn-success simple-tooltip" data-placement="top" data-toggle="tooltip" title="Set OTS as Complete"><i class="fas fa-check"></i></button>
-                                        </span>
-                                        <a href="/dashboard/training/ots-center/cancel/{{ $o->id }}" class="btn btn-warning simple-tooltip" data-toggle="tooltip" title="Cancel OTS"><i class="fas fa-ban"></i></a>
-                                    @endif
-                                </td>
-                            </tr>
+            @if($ots_accepted->count() > 0)
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Date of Recommendation</th>
+                            <th scope="col">Controller Name</th>
+                            <th scope="col">Recommender Name</th>
+                            <th scope="col">Instructor</th>
+                            <th scope="col">Position</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    @foreach($ots_accepted as $o)
+                        <tr>
+                            <td>{{ $o->recommended_on }}</td>
+                            <td>{{ $o->controller_name }}</td>
+                            <td>{{ $o->recommender_name }}</td>
+                            <td>{{ $o->ins_name }}</td>
+                            <td>{{ $o->position_name }}</td>
+                            <td>
+                                @if($o->ins_id == Auth::id() || Auth::user()->isAbleTo('snrStaff'))
+                                    <span data-toggle="modal" data-target="#completeOTS{{ $o->id }}">
+                                        <button type="button" class="btn btn-success simple-tooltip" data-placement="top" data-toggle="tooltip" title="Set OTS as Complete"><i class="fas fa-check"></i></button>
+                                    </span>
+                                    <a href="/dashboard/training/ots-center/cancel/{{ $o->id }}" class="btn btn-warning simple-tooltip" data-toggle="tooltip" title="Cancel OTS"><i class="fas fa-ban"></i></a>
+                                @endif
+                            </td>
+                        </tr>
 
-                            <div class="modal fade" id="completeOTS{{ $o->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title">Upload OTS Report</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        {{ html()->form()->route('completeOTS', [$o->id])->acceptsFiles()->open() }}
-                                        @csrf
-                                        <div class="modal-body">
-                                            <div class="form-group">
-                                                <label for="result">Result</label>
-                                                {{ html()->select('result', [2 => 'Pass', 3 => 'Fail'], null)->placeholder('Select Result')->class(['form-control']) }}
-                                            </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                            <button action="submit" class="btn btn-success">Set Complete</button>
-                                        </div>
-                                        {{ html()->form()->close() }}
+                        <div class="modal fade" id="completeOTS{{ $o->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Upload OTS Report</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
                                     </div>
+                                    {{ html()->form()->route('completeOTS', [$o->id])->acceptsFiles()->open() }}
+                                    @csrf
+                                    <div class="modal-body">
+                                        <div class="form-group">
+                                            <label for="result">Result</label>
+                                            {{ html()->select('result', [2 => 'Pass', 3 => 'Fail'], null)->placeholder('Select Result')->class(['form-control']) }}
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button action="submit" class="btn btn-success">Set Complete</button>
+                                    </div>
+                                    {{ html()->form()->close() }}
                                 </div>
                             </div>
-                        @endforeach
-                    @else
-                        <tr>
-                            <td colspan="6">No accepted OTS exam recommendations found.</td>
-                        </tr>
-                    @endif
+                        </div>
+                    @endforeach
                 </thead>
             </table>
+            @else
+                @include('inc.empty_state', ['header' => 'No Accepted Recommendations', 'body' => 'No accepted OTS recommendations at this time', 'icon' => 'fa-solid fa-file'])
+            @endif
         </div>
         <div role="tabpanel" class="tab-pane" id="complete">
             <table class="table">
