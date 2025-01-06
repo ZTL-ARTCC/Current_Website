d69b3acedab2bfc59d17f320379e76b32369456e
Ian Cowan <ian@cowan.aero>
2025-01-06 14:50:05 -0500

    #698 Empty state for scenery management

--- resources/views/dashboard/admin/scenery/index.blade.php
+++ resources/views/dashboard/admin/scenery/index.blade.php
@@ -77,7 +77,7 @@ Scenery Management
                     </thead>
                 </table>
             @else
-                <p>No scenery available for this simulator type.</p>
+                @include('inc.empty_state', ['header' => 'No Scenery', 'body' => 'No scenery currently exists for this simulator type.', 'icon' => 'fa fa-plane'])
             @endif
         </div>
         @endforeach
