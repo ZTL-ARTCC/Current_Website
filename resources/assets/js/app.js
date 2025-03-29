97f48acd94a847e53b2ca0a5aaf84354ce5b4a34
kjporter <kporter@kplink.net>
2025-03-29 12:26:43 -0400

    Update app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,6 @@ require("./ckeditor.js");
 require("./bootstrap.js");
 require("./helpers.js");
 
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/brands';
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/brands";
