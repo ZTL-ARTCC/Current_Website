34678ea82c474feb3ad0dd89e03463219495c8df
Evanston09 <evanhkim09@gmail.com>
2025-03-31 21:32:17 -0400

    Fix formatting anomalies

--- app/TrainerFeedback.php
+++ app/TrainerFeedback.php
@@ -27,7 +27,7 @@ class TrainerFeedback extends Model {
     }
 
     public function getTrainingMethodTextAttribute() {
-        switch($this->training_method) {
+        switch ($this->training_method) {
             case 0: return 'Theory';
             case 1: return 'Sweatbox';
             case 2: return 'Live Network';
