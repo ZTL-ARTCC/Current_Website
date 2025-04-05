d53b5acae964f1e4b3c92b4567d2017d0d332e2e
kjporter <kporter@kplink.net>
2025-04-05 14:50:42 -0400

    Highlights error with color

--- resources/views/inc/empty_state.blade.php
+++ resources/views/inc/empty_state.blade.php
@@ -1,9 +1,24 @@
+@php
+$text_style = '';
+if(isset($color)) {
+    switch($color) {
+        case 'red' : $text_style = 'text-danger';
+        break;
+        case 'yellow' : $text_style = 'text-warning';
+        break;
+        case 'green' : $text_style = 'text-success';
+        break;
+        case 'blue' : $text_style = 'text-primary';
+        break;
+    }
+}
+@endphp
 <div class="card mb-3">
     <div class="card-body text-center">
         <h4>{!! $header !!}</h4>
-        <p>{!! $body !!}</p>
+        <p class="{{ $text_style }}">{!! $body !!}</p>
         @if(isset($icon))
-            <h4 class="{!! $icon !!}"></h4>
+            <h4 class="{!! $icon !!} {{ $text_style }}"></h4>
         @endif
     </div>
 </div>
