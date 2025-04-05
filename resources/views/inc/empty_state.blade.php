bc4beb87507e5ebeb781fd3c5dff072fde999814
kjporter <kporter@kplink.net>
2025-04-05 15:47:42 -0400

    Update style injection

--- resources/views/inc/empty_state.blade.php
+++ resources/views/inc/empty_state.blade.php
@@ -1,12 +1,13 @@
 @php
-$text_style = (isset($class)) ? $class : '';
+$header_style = (isset($header_class)) ? $header_class : '';
+$body_style = (isset($body_class)) ? $body_class : '';
 @endphp
 <div class="card mb-3">
     <div class="card-body text-center">
-        <h4>{!! $header !!}</h4>
-        <p class="{{ $text_style }}">{!! $body !!}</p>
+        <h4 class="{{ $header_style }}">{!! $header !!}</h4>
+        <p class="{{ $body_style }}">{!! $body !!}</p>
         @if(isset($icon))
-            <h4 class="{!! $icon !!} {{ $text_style }}"></h4>
+            <h4 class="{!! $icon !!} {{ $body_style }}"></h4>
         @endif
     </div>
 </div>
