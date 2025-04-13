bd369917db4828ff3208d5d670520887841e59f0
kjporter <kporter@kplink.net>
2025-04-12 20:49:03 -0400

    Update import directives

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,3 +11,5 @@ require("./helpers.js");
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/brands";
+import "@fortawesome/fontawesome-free/css/all.css";
+config.autoAddCss = false;
