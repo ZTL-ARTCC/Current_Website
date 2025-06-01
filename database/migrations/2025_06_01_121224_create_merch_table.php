4219ffb088dfb8905c91696a3ec34c0306a77caf
kjporter <kporter@kplink.net>
2025-06-01 11:45:43 -0400

    Fix formatting

--- database/migrations/2025_06_01_121224_create_merch_table.php
+++ database/migrations/2025_06_01_121224_create_merch_table.php
@@ -4,13 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('merch', function (Blueprint $table) {
             $table->id();
             $table->string('title');
@@ -24,8 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('merch');
     }
 };
