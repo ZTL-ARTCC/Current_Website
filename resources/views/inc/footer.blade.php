8ebdfe7b3a68702beb03c8082c7fcc70cdcca9f6
kjporter <kporter@kplink.net>
2025-08-10 07:58:00 -0400

    Refactor deprecated code elements

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,8 +1,7 @@
 <footer id="myFooter">
     <div class="footer-social">
         <p>Visit our partners:</p>
-        <a style="position: relative; top: -10px" href="https://www.teamspeak.com/?tour=yes" target="_blank"><img width="200" src="/photos/partner-ts.png"></a> &nbsp;&nbsp;&nbsp; <a style="position: relative; top: -10px" href="https://www.thepilotclub.org/" target="_blank"><img height="100" src="/photos/partner-tpc.png"></a>
-        <br><br>
+        <a style="position: relative; top: -10px" href="https://www.teamspeak.com/?tour=yes" target="_blank"><img width="200" class="m-2" src="/photos/partner-ts.png"></a><a style="position: relative; top: -10px" href="https://www.thepilotclub.org/" target="_blank"><img height="100" class="m-2" src="/photos/partner-tpc.png"></a>
         <p><i>For entertainment purposes only. Do not use for real world purposes. Part of the VATSIM Network.</i></p>
     </div>
     <div class="container">
