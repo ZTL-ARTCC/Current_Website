f925588c8b152c6bd6d35b9ec5fe1108465ee3bf
kjporter <kporter@kplink.net>
2025-10-25 16:47:55 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
+import path from 'path';
+import glob from "glob";
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/assets/sass/app.scss',
-                'resources/assets/js/app.js',
+                ...glob.sync("resources/assets/sass/*.scss"),
+                ...glob.sync("resources/assets/js/*.js*"),
             ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
