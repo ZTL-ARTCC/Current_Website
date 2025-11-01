aea488faa6dbe1280077ba9d59d7814303ecbc4d
kjporter <kporter@kplink.net>
2025-11-01 09:27:01 -0400

    Add CKEditor5 Vite Plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
+import { createRequire } from 'node:module';
+const require = createRequire( import.meta.url );
 import { defineConfig } from 'vite';
+import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
 import path from 'path';
 import glob from "glob";
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        ckeditor5({
+            theme: require.resolve('@ckeditor/ckeditor5-theme-lark')
+        }),
         laravel({
             input: [
                 ...glob.sync("resources/assets/sass/*.scss"),
