98427c76ec562216e48726991e8bbc1b8f3aec44
Ian Cowan <ian@cowan.aero>
2026-02-09 22:01:56 -0500

    #863 Fix controller dash navbar formatting

--- resources/views/inc/dashboard_head.blade.php
+++ resources/views/inc/dashboard_head.blade.php
@@ -1,26 +1,23 @@
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
         <a class="navbar-brand" href="/dashboard">
             @include('inc.logo', ['color' => 'black'])
         </a>
         <div class="d-flex justify-content-start ms-5 collapse navbar-collapse">
-
-                {{ html()->form()->route('searchAirport')->class(['row','row-cols-lg-auto'])->open() }}
-                    <div class="col-12 input-group">
-                        {{ html()->text('apt', null)->placeholder('Search Airport ICAO')->class(['form-control']) }}
-                        &nbsp;
-                        <button class="btn btn-success" type="submit">Search</button>
-                    </div>
-                {{ html()->form()->close() }}
-</div>
-
-            <ul class="navbar-nav ms-auto">
-			<a class="nav-link {{ Nav::isRoute('controller_dash_home') }}" href="/dashboard">Dashboard Home</a>
-                <li class="nav-item dropdown">
-                    <a class="nav-link" style="pointer-events:none">{{ Auth::user()->full_name }} - {{ Auth::user()->rating_short }}</a>
-                </li>
-            </ul>
+            {{ html()->form()->route('searchAirport')->class(['row','row-cols-lg-auto'])->open() }}
+                <div class="col-12 input-group">
+                    {{ html()->text('apt', null)->placeholder('Search Airport ICAO')->class(['form-control']) }}
+                    &nbsp;
+                    <button class="btn btn-success" type="submit">Search</button>
+                </div>
+            {{ html()->form()->close() }}
         </div>
-</div>
-    </nav>
+        <ul class="navbar-nav ms-auto">
+            <a class="nav-link {{ Nav::isRoute('controller_dash_home') }}" href="/dashboard">Dashboard Home</a>
+            <li class="nav-item dropdown">
+                <a class="nav-link" style="pointer-events:none">{{ Auth::user()->full_name }} - {{ Auth::user()->rating_short }}</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
