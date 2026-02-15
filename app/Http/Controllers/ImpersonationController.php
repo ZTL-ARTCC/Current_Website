9c665cd2e1e01e23ed5d077fceb8f0159f8a89e2
Ian Cowan <ian@cowan.aero>
2026-02-15 10:53:51 -0500

    #863 Disable changing impersonation when impersonating

--- app/Http/Controllers/ImpersonationController.php
+++ app/Http/Controllers/ImpersonationController.php
@@ -9,8 +9,13 @@ use Illuminate\Http\Request;
 class ImpersonationController extends Controller {
     public function start(Request $request) {
         $user = User::find($request->user_id);
+        $is_impersonating = session()->has(SessionVariables::IMPERSONATE->value);
         if (is_null($user)) {
             return redirect()->back()->with(SessionVariables::ERROR->value, 'That user does not exist');
+        }
+
+        if ($is_impersonating) {
+            return redirect()->back()->with(SessionVariables::ERROR->value, 'You must first stop impersonating your current user before beginning a new session');
         }
 
         session()->put(SessionVariables::IMPERSONATE->value, $user->id);
