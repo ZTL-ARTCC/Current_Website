df6337fc8b7eb3791af387e90eed049850d18c23
Kyle <kporter@kplink.net>
2026-03-12 17:57:24 -0400

    Fix typo

--- resources/views/inc/impersonation_warning.blade.php
+++ resources/views/inc/impersonation_warning.blade.php
@@ -1,6 +1,6 @@
 @if($is_impersonating && toggleEnabled($FeatureToggles::IMPERSONATION))
     <br>
     <a class="d-block alert alert-warning" href="/dashboard/admin/impersonation/stop">
-        WARNING: You are currently impersonating a user. Use extreme caution as any action you perform will be performed at that user, and will be tracked as so with you as the impersonator. This should be used for debugging and development only! Click this warning at any time to end impersonation.
+        WARNING: You are currently impersonating a user. Use extreme caution as any action you perform will be performed as that user, and will be tracked as so with you as the impersonator. This should be used for debugging and development only! Click this warning at any time to end impersonation.
     </a>
 @endif
