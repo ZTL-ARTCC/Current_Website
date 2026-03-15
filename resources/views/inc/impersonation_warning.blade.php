9d154507e9663797c9f875a9b1718862421ab2fb
Kyle <kporter@kplink.net>
2026-03-14 21:48:59 -0400

    Update impersonation_warning.blade.php

--- resources/views/inc/impersonation_warning.blade.php
+++ resources/views/inc/impersonation_warning.blade.php
@@ -1,4 +1,4 @@
-@if($is_impersonating && toggleEnabled($FeatureToggles::IMPERSONATION))
+@if(isset($is_impersonating) && $is_impersonating && toggleEnabled($FeatureToggles::IMPERSONATION))
     <br>
     <a class="d-block alert alert-warning" href="/dashboard/admin/impersonation/stop">
         WARNING: You are currently impersonating a user. Use extreme caution as any action you perform will be performed as that user, and will be tracked as so with you as the impersonator. This should be used for debugging and development only! Click this warning at any time to end impersonation.
