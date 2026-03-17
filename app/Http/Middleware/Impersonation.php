101d7df13f35524fb69f194bcb74d35691e236ff
Ian Cowan <ian@cowan.aero>
2026-03-16 20:36:30 -0400

    #863 Fix middleware logic for browser test

--- app/Http/Middleware/Impersonation.php
+++ app/Http/Middleware/Impersonation.php
@@ -16,7 +16,7 @@ class Impersonation {
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response {
-        if (toggleEnabled(FeatureToggles::IMPERSONATION) && session()->has(SessionVariables::IMPERSONATE->value) && Auth::check() && Auth::user()->hasRole('wm') || Auth::user()->hasRole('awm')) {
+        if (toggleEnabled(FeatureToggles::IMPERSONATION) && session()->has(SessionVariables::IMPERSONATE->value) && Auth::check() && (Auth::user()->hasRole('wm') || Auth::user()->hasRole('awm'))) {
             session()->put(SessionVariables::IMPERSONATING_USER->value, Auth::id());
             Auth::onceUsingId(session(SessionVariables::IMPERSONATE->value));
         }
