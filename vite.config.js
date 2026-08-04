4157132a8e989d4a4aeb8dd690ccfa947e1a726c
core <core@coredoes.dev>
2026-08-03 23:00:47 -0400

    noiss: vite manifest fixes (include all supported image types)

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
             input: [
                 ...globSync("resources/assets/sass/*.scss"),
                 ...globSync("resources/assets/js/*.js*"),
-		...globSync("resources/assets/img/{*,**/*}.{png,svg,jpg,webp,avif}"),
+		...globSync("resources/assets/img/{*,**/*}.{png,jpg,jpeg,jfif,pjpeg,pjp,gif,svg,ico,webp,avif,cur,ani}"),
             ],
             refresh: true,
         }),
