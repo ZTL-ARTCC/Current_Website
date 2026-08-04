c7413cf5a2999c504bb796caf8a6a1c6183333b2
core <core@coredoes.dev>
2026-08-03 22:23:25 -0400

    noiss Vite v8.0

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,23 @@
 import { createRequire } from 'node:module';
 const require = createRequire( import.meta.url );
 import { defineConfig } from 'vite';
-import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
 import path from 'path';
-import glob from "glob";
+import { globSync } from "glob";
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        ckeditor5({
-            theme: require.resolve('@ckeditor/ckeditor5-theme-lark')
-        }),
         laravel({
             input: [
-                ...glob.sync("resources/assets/sass/*.scss"),
-                ...glob.sync("resources/assets/js/*.js*"),
+                ...globSync("resources/assets/sass/*.scss"),
+                ...globSync("resources/assets/js/*.js*"),
             ],
             refresh: true,
         }),
     ],
     resolve: {
         alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~bootstrap': path.resolve(import.meta.dirname, 'node_modules/bootstrap'),
         }
     },
 });
