b5197541efeec46a6759af340581319a011d050e
Kyle <kporter@kplink.net>
2026-08-21 18:44:03 -0700

    Update PreventBackHistory.php

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -14,7 +14,7 @@ class PreventBackHistory {
      */
     public function handle(Request $request, Closure $next): Response {
         $response = $next($request);
-        $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+        $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0');
         $response->headers->set('Pragma', 'no-cache');
         $response->headers->set('Expires', 'Sun, 02 Jan 1990 00:00:00 GMT');
         return $response;
