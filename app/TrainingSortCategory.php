38ced2eca68fca93fff2311d052a0b9ef928c54f
Kyle <kporter@kplink.net>
2026-09-22 22:14:55 -0700

    Update TrainingSortCategory.php

--- app/TrainingSortCategory.php
+++ app/TrainingSortCategory.php
@@ -13,4 +13,24 @@ class TrainingSortCategory extends Model {
         'scheddy_booking_map'
     ];
     public $timestamps = false;
+
+    public static function legacyTicketTypes(int $category): int {
+        if (config('vatusa.facility') != 'ZTL') {
+            return self::first()->id;
+        }
+        return match ($category) {
+            11  => 104,
+            18  => 108,
+            27  => 113,
+            31  => 115,
+            32  => 115,
+            42  => 121,
+            103 => 104,
+            107 => 108,
+            112 => 113,
+            114 => 115,
+            120 => 121,
+            default => $category,
+        };
+    }
 }
